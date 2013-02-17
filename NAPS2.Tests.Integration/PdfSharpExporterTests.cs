c1d98082efeadf2ce5b7da40285bd1dccd52101c
ben-cyanfish <ben.cyanfish@gmail.com>
2013-02-17 18:29:17 -0500

    Added test projects

---
+++ NAPS2.Tests.Integration/PdfSharpExporterTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using NAPS;
+using NUnit.Framework;
+
+namespace NAPS2.Tests.Integration
+{
+    [TestFixture]
+    public class PdfSharpExporterTests : IPdfExporterTests
+    {
+        public override void SetUp()
+        {
+            base.SetUp();
+            pdfExporter = new PdfSharpExporter();
+        }
+    }
+}
---
+++ NAPS2.Tests.Unit/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace NAPS2.Tests.Unit
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
