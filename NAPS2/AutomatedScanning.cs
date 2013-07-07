bee4d53ed188ce11af507dd8a404641615e3deb9
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2013-07-07 11:22:54 -0400

    Added reference to the CommandLineParser library. Created some argument specifications.

---
+++ NAPS2/AutomatedScanning.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NAPS2
+{
+    public class AutomatedScanning
+    {
+    }
+}
---
+++ NAPS2/AutomatedScanningParameters.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using CommandLine;
+
+namespace NAPS2
+{
+    public class AutomatedScanningParameters
+    {
+        [Option('o', "output", Required = true, HelpText = "The path to the file to save.")]
+        public string OutputPath { get; set; }
+
+        [Option('p', "profile", HelpText = "The name of the profile to use for scanning. If not specified, the most-recently-used profile is selected.")]
+        public string ProfileName { get; set; }
+
+        [Option('v', "verbose", HelpText = "Display progress information. If not specified, no output is displayed if the scan is successful.")]
+        public bool Verbose { get; set; }
+
+        [Option('n', "number", DefaultValue = 1, HelpText = "The number of pages to scan.")]
+        public int Number { get; set; }
+
+        [Option('d', "delay", DefaultValue = 0, HelpText = "The delay (in milliseconds) before each page is scanned.")]
+        public int Delay { get; set; }
+
+        // TODO: PDF/Image/ImageFormat
+    }
+}
