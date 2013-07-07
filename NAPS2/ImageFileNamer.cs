06c244c2d5087a14a4a8f0615ff2baef56bce22e
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2013-07-07 10:10:11 -0400

    Changed the framework target from 3.5CP to 4.0CP. Changed the platform target for Tests to x86 to be compatible with the change to the main project.

---
+++ NAPS2/ImageFileNamer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NAPS2
+{
+    public class ImageFileNamer
+    {
+        public string GetName()
+        {
+            return null;
+        }
+    }
+}
