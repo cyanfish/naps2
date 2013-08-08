12c56414df9287eba5696a9d1d7503bbcd13bdfe
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2013-08-08 17:31:03 -0400

    PUBLISH (2.3)

--- NAPS2/Setup/build-standalone.bat
+++ NAPS2/Setup/build-standalone.bat
@@ -1,6 +1,6 @@
 @echo off
 cd %~dp0
-set VERSION=2.2
+set VERSION=2.3
 set OUTFOLDER=..\publish\%VERSION%
 set TEMPFOLDER=%OUTFOLDER%\naps2-%VERSION%-standalone
 set OUTFILE=..\publish\%VERSION%\naps2-%VERSION%-standalone.zip
--- NAPS2/Setup/copy-msi.bat
+++ NAPS2/Setup/copy-msi.bat
@@ -1,6 +1,6 @@
 @echo off
 cd %~dp0
-set VERSION=2.2
+set VERSION=2.3
 set OUTFOLDER=..\publish\%VERSION%
 set OUTFILE=%OUTFOLDER%\naps2-%VERSION%-setup.msi
 set SETUPBINFOLDER=..\..\NAPS2.Setup\bin\Release
