9d1d29d329d870f20216ef7a86907c5670005934
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2013-08-20 02:32:15 -0400

    Added substantial commenting to the LayoutManager API. Added a check for duplicate bindings. Improved DependencyWalker to detect dependencies in more cases.

--- NAPS2/LayoutManager.cs
+++ NAPS2/LayoutManager.cs
@@ -8,27 +8,78 @@ using System.Windows.Forms;
 
 namespace NAPS2
 {
+    /// <summary>
+    /// A layout manager for WinForms that takes advantage of the relative sizes and positions of controls from the WinForms designer.
+    /// When the LayoutManager is activated, the layout will initially stay the same and then smoothly change as the form is resized.
+    /// Note that the layout manager is designed to be "stretch-only", i.e. it won't change the flow of the controls.
+    /// 
+    /// LayoutManager provides fluent syntax that is typically used similarly to the following:
+    ///     new LayoutManager(this)                 // Init layout manager with a reference to the form
+    ///         .Bind(control1)                     // Start describing bindings for control1
+    ///             .WidthToForm()                  // control1's width should change with the form's width
+    ///         .Bind(control1, control2)           // Start describing bindings for control1 AND control2
+    ///             .Right(() => control3.Right)    // Both control1 and control2's right sides should stay aligned with control3's right side
+    ///             .BottomToForm()                 // Both control1 and control2's bottom side should stay aligned with the form's bottom side
+    ///         .Activate();                        // Start laying out controls in response to Resize events
+    /// 
+    /// Note that the following bindings (for example) are equivalent:
+    ///     .LeftToForm()
+    ///     .LeftTo(() => form.Left)
+    /// 
+    /// The more general form of binding (e.g. LeftTo) can be used with arbitrary expressions.
+    /// Note that dependency tracking will only work if the dependent controls are accessed in the expression.
+    /// Examples:
+    ///     .WidthTo(() => form.Width / 2)                                  // OK
+    ///     .LeftTo(() => control1.Left + control2.Width + SomeFunction())  // OK if SomeFunction doesn't depend on the size or position of any controls other than control1 or control2
+    ///     .LeftTo(() => SomeFunction(control1))                           // OK
+    ///     .LeftTo(() => SomeFunctionDependingOnControl1())                // NOT OK
+    /// </summary>
     public class LayoutManager
     {
         private readonly List<KeyValuePair<Control, HashSet<Binding>>> activatedControlBindings = new List<KeyValuePair<Control, HashSet<Binding>>>();
 
+        /// <summary>
+        /// Initializes an instance of the LayoutManager class that will layout the controls of the given form.
+        /// </summary>
+        /// <param name="form">The form whose controls will be laid out.</param>
         public LayoutManager(Form form)
         {
             Form = form;
             Bindings = new List<Binding>();
         }
 
+        /// <summary>
+        /// Gets the form whose layout the LayoutManager is managing.
+        /// </summary>
         public Form Form { get; private set; }
 
+        /// <summary>
+        /// Gets a list of bindings belonging to the LayoutManager.
+        /// Changes to this list are applied at activation.
+        /// </summary>
         public List<Binding> Bindings { get; private set; }
 
+        /// <summary>
+        /// Gets a value indicating whether the LayoutManager has been activated (but not deacivated since).
+        /// </summary>
         public bool Activated { get; private set; }
 
+        /// <summary>
+        /// Begins the description of a set of bindings for the given controls.
+        /// </summary>
+        /// <param name="controls">The controls to which to apply the following bindings.</param>
+        /// <returns>An object that provides an interface to describe bindings.</returns>
         public BindingSyntax Bind(params Control[] controls)
         {
             return new BindingSyntax(this, controls);
         }
 
+        /// <summary>
+        /// Activates the LayoutManager. This performs dependency analysis and starts listening for the form's Resize event.
+        /// The relative positions of bound controls is determined by the offset at activation.
+        /// For example, if a control's right side is bound to the form's right side and its right side is 12 pixels to the
+        /// left of the form's right side, that 12 pixel difference will be maintained by the LayoutManager.
+        /// </summary>
         public void Activate()
         {
             if (!Activated)
@@ -103,6 +154,9 @@ namespace NAPS2
             }
         }
 
+        /// <summary>
+        /// Deactivates the LayoutManager. Stops listening for the form's Resize event.
+        /// </summary>
         public void Deactivate()
         {
             if (Activated)
@@ -118,8 +172,15 @@ namespace NAPS2
             {
                 var control = pair.Key;
                 var bindings = pair.Value;
+                var bindingTypesUsed = new bool[(int)BindingType.Bottom + 1];
                 foreach (var binding in bindings.OrderBy(x => x.BindingType == BindingType.Right || x.BindingType == BindingType.Bottom ? 1 : 0))
                 {
+                    if (bindingTypesUsed[(int)binding.BindingType])
+                    {
+                        throw new InvalidOperationException("Duplicate " + binding.BindingType + " binding for control " + control.Name + ".");
+                    }
+                    bindingTypesUsed[(int)binding.BindingType] = true;
+
                     int offset = binding.InitialValue - binding.InitialDependentValue;
                     switch (binding.BindingType)
                     {
@@ -134,7 +195,7 @@ namespace NAPS2
                             bool hasLeftBinding = bindings.Any(x => x.BindingType == BindingType.Left);
                             if (hasWidthBinding && hasLeftBinding)
                             {
-                                throw new InvalidOperationException("The layout bindings for control " + control.Name + " are overspecified.");
+                                throw new InvalidOperationException("The layout bindings (Left/Right/Width) for control " + control.Name + " are overspecified.");
                             }
                             else if (hasLeftBinding)
                             {
@@ -150,7 +211,7 @@ namespace NAPS2
                             bool hasTopBinding = bindings.Any(x => x.BindingType == BindingType.Top);
                             if (hasHeightBinding && hasTopBinding)
                             {
-                                throw new InvalidOperationException("The layout bindings for control " + control.Name + " are overspecified.");
+                                throw new InvalidOperationException("The layout bindings (Top/Bottom/Height) for control " + control.Name + " are overspecified.");
                             }
                             else if (hasTopBinding)
                             {
@@ -166,22 +227,36 @@ namespace NAPS2
             }
         }
 
-        public class DependencyWalker : ExpressionVisitor
+        private class DependencyWalker : ExpressionVisitor
         {
             public DependencyWalker()
             {
-                Dependencies = new List<Control>();
+                Dependencies = new HashSet<Control>();
             }
 
-            public List<Control> Dependencies { get; private set; }
+            public HashSet<Control> Dependencies { get; private set; }
 
             protected override Expression VisitMember(MemberExpression node)
             {
-                if (typeof(Control).IsAssignableFrom(node.Expression.Type))
-                {
-                    Dependencies.Add(((Expression<Func<Control>>)Expression.Lambda(Expression.Convert(node.Expression, typeof(Control)))).Compile()());
-                }
+                AddDeps(node.Expression);
+                AddDeps(node);
                 return base.VisitMember(node);
+            }
+
+            protected override Expression VisitConstant(ConstantExpression node)
+            {
+                AddDeps(node);
+                return base.VisitConstant(node);
+            }
+
+            private void AddDeps(Expression expr)
+            {
+                if (typeof(Control).IsAssignableFrom(expr.Type))
+                {
+                    Dependencies.Add(
+                        ((Expression<Func<Control>>)Expression.Lambda(Expression.Convert(expr, typeof(Control))))
+                            .Compile()());
+                }
             }
         }
 
@@ -281,96 +356,161 @@ namespace NAPS2
                 this.controls = controls;
             }
 
+            /// <summary>
+            /// Begins the description of a new set of bindings for the given controls, and ends the previous description of bindings.
+            /// </summary>
+            /// <param name="controls">The controls to which to apply the following bindings.</param>
+            /// <returns>An object that provides an interface to describe bindings.</returns>
             public BindingSyntax Bind(params Control[] controls)
             {
                 return new BindingSyntax(layoutManager, controls);
             }
 
+            /// <summary>
+            /// Activates the LayoutManager. This performs dependency analysis and starts listening for the form's Resize event.
+            /// The relative positions of bound controls is determined by the offset at activation.
+            /// For example, if a control's right side is bound to the form's right side and its right side is 12 pixels to the
+            /// left of the form's right side, that 12 pixel difference will be maintained by the LayoutManager.
+            /// </summary>
             public LayoutManager Activate()
             {
                 layoutManager.Activate();
                 return layoutManager;
             }
 
-            public BindingSyntax WidthTo(Expression<Func<int>> widthFunc)
+            /// <summary>
+            /// Binds the width of each of the controls in the current set to the value of the specified expression.
+            /// </summary>
+            /// <param name="func">The expression whose value will be bound to the controls' property.</param>
+            /// <returns>An object that provides an interface to describe bindings.</returns>
+            public BindingSyntax WidthTo(Expression<Func<int>> func)
             {
                 foreach (var control in controls)
                 {
-                    layoutManager.Bindings.Add(new Binding(control, BindingType.Width, widthFunc));
+                    layoutManager.Bindings.Add(new Binding(control, BindingType.Width, func));
                 }
                 return this;
             }
 
-            public BindingSyntax HeightTo(Expression<Func<int>> widthFunc)
+            /// <summary>
+            /// Binds the height of each of the controls in the current set to the value of the specified expression.
+            /// </summary>
+            /// <param name="func">The expression whose value will be bound to the controls' property.</param>
+            /// <returns>An object that provides an interface to describe bindings.</returns>
+            public BindingSyntax HeightTo(Expression<Func<int>> func)
             {
                 foreach (var control in controls)
                 {
-                    layoutManager.Bindings.Add(new Binding(control, BindingType.Height, widthFunc));
+                    layoutManager.Bindings.Add(new Binding(control, BindingType.Height, func));
                 }
                 return this;
             }
 
-            public BindingSyntax LeftTo(Expression<Func<int>> widthFunc)
+            /// <summary>
+            /// Binds the left side of each of the controls in the current set to the value of the specified expression.
+            /// </summary>
+            /// <param name="func">The expression whose value will be bound to the controls' property.</param>
+            /// <returns>An object that provides an interface to describe bindings.</returns>
+            public BindingSyntax LeftTo(Expression<Func<int>> func)
             {
                 foreach (var control in controls)
                 {
-                    layoutManager.Bindings.Add(new Binding(control, BindingType.Left, widthFunc));
+                    layoutManager.Bindings.Add(new Binding(control, BindingType.Left, func));
                 }
                 return this;
             }
 
-            public BindingSyntax RightTo(Expression<Func<int>> widthFunc)
+            /// <summary>
+            /// Binds the right side of each of the controls in the current set to the value of the specified expression.
+            /// </summary>
+            /// <param name="func">The expression whose value will be bound to the controls' property.</param>
+            /// <returns>An object that provides an interface to describe bindings.</returns>
+            public BindingSyntax RightTo(Expression<Func<int>> func)
             {
                 foreach (var control in controls)
                 {
-                    layoutManager.Bindings.Add(new Binding(control, BindingType.Right, widthFunc));
+                    layoutManager.Bindings.Add(new Binding(control, BindingType.Right, func));
                 }
                 return this;
             }
 
-            public BindingSyntax TopTo(Expression<Func<int>> widthFunc)
+            /// <summary>
+            /// Binds the top side of each of the controls in the current set to the value of the specified expression.
+            /// </summary>
+            /// <param name="func">The expression whose value will be bound to the controls' property.</param>
+            /// <returns>An object that provides an interface to describe bindings.</returns>
+            public BindingSyntax TopTo(Expression<Func<int>> func)
             {
                 foreach (var control in controls)
                 {
-                    layoutManager.Bindings.Add(new Binding(control, BindingType.Top, widthFunc));
+                    layoutManager.Bindings.Add(new Binding(control, BindingType.Top, func));
                 }
                 return this;
             }
 
-            public BindingSyntax BottomTo(Expression<Func<int>> widthFunc)
+            /// <summary>
+            /// Binds the bottom side of each of the controls in the current set to the value of the specified expression.
+            /// </summary>
+            /// <param name="func">The expression whose value will be bound to the controls' property.</param>
+            /// <returns>An object that provides an interface to describe bindings.</returns>
+            public BindingSyntax BottomTo(Expression<Func<int>> func)
             {
                 foreach (var control in controls)
                 {
-                    layoutManager.Bindings.Add(new Binding(control, BindingType.Bottom, widthFunc));
+                    layoutManager.Bindings.Add(new Binding(control, BindingType.Bottom, func));
                 }
                 return this;
             }
 
+            /// <summary>
+            /// Binds the width of each of the controls in the current set to the width of the form.
+            /// </summary>
+            /// <returns>An object that provides an interface to describe bindings.</returns>
             public BindingSyntax WidthToForm()
             {
                 return WidthTo(() => layoutManager.Form.Width);
             }
 
+            /// <summary>
+            /// Binds the height of each of the controls in the current set to the height of the form.
+            /// </summary>
+            /// <returns>An object that provides an interface to describe bindings.</returns>
             public BindingSyntax HeightToForm()
             {
                 return HeightTo(() => layoutManager.Form.Height);
             }
 
+            /// <summary>
+            /// Binds the left side of each of the controls in the current set to the left side of the form.
+            /// </summary>
+            /// <returns>An object that provides an interface to describe bindings.</returns>
             public BindingSyntax LeftToForm()
             {
                 return LeftTo(() => layoutManager.Form.Left);
             }
 
+            /// <summary>
+            /// Binds the right side of each of the controls in the current set to the right side of the form.
+            /// </summary>
+            /// <returns>An object that provides an interface to describe bindings.</returns>
             public BindingSyntax RightToForm()
             {
                 return RightTo(() => layoutManager.Form.Right);
             }
 
+            /// <summary>
+            /// Binds the top side of each of the controls in the current set to the top side of the form.
+            /// </summary>
+            /// <returns>An object that provides an interface to describe bindings.</returns>
             public BindingSyntax TopToForm()
             {
                 return TopTo(() => layoutManager.Form.Top);
             }
 
+            /// <summary>
+            /// Binds the bottom side of each of the controls in the current set to the bottom side of the form.
+            /// </summary>
+            /// <returns>An object that provides an interface to describe bindings.</returns>
             public BindingSyntax BottomToForm()
             {
                 return BottomTo(() => layoutManager.Form.Bottom);
