9679a17db9a8aecde48eb3c3bfc7b221faf8ed74
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2013-09-21 09:24:06 -0400

    Added auto updater types to CommonModule

--- NAPS2/DI/CommonModule.cs
+++ NAPS2/DI/CommonModule.cs
@@ -12,6 +12,7 @@ using NAPS2.Scan.Images;
 using NAPS2.Scan.Stub;
 using NAPS2.Scan.Twain;
 using NAPS2.Scan.Wia;
+using NAPS2.Update;
 using Ninject.Modules;
 using NLog;
 
@@ -21,17 +22,19 @@ namespace NAPS2.DI
     {
         public override void Load()
         {
+
+            // Import
             Bind<IScannedImageImporter>().To<ScannedImageImporter>().When(x => true); // Fix so that this binding is only used when no name is specified
             Bind<IScannedImageImporter>().To<PdfSharpImporter>().Named("pdf");
             Bind<IScannedImageImporter>().To<ImageImporter>().Named("image");
-            Bind<IScannedImageFactory>().To<FileBasedScannedImageFactory>();
-            Bind<IScanPerformer>().To<ScanPerformer>();
-            Bind<IProfileManager>().To<ProfileManager>().InSingletonScope();
-            Bind<AppConfigManager>().ToSelf().InSingletonScope();
-            Bind<UserConfigManager>().ToSelf().InSingletonScope();
+
+            // Export
             Bind<IPdfExporter>().To<PdfSharpExporter>();
             Bind<IEmailer>().To<MapiEmailer>();
-            Bind<Logger>().ToMethod(ctx => LoggerFactory.Current.GetLogger()).InSingletonScope();
+
+            // Scan
+            Bind<IScannedImageFactory>().To<FileBasedScannedImageFactory>();
+            Bind<IScanPerformer>().To<ScanPerformer>();
 #if DEBUG && false
             Bind<IScanDriver>().To<StubScanDriver>().Named(WiaScanDriver.DRIVER_NAME).WithConstructorArgument("driverName", WiaScanDriver.DRIVER_NAME);
             Bind<IScanDriver>().To<StubScanDriver>().Named(TwainScanDriver.DRIVER_NAME).WithConstructorArgument("driverName", TwainScanDriver.DRIVER_NAME);
@@ -39,6 +42,28 @@ namespace NAPS2.DI
             Bind<IScanDriver>().To<WiaScanDriver>().Named(WiaScanDriver.DRIVER_NAME);
             Bind<IScanDriver>().To<TwainScanDriver>().Named(TwainScanDriver.DRIVER_NAME);
 #endif
+
+            // Config
+            Bind<IProfileManager>().To<ProfileManager>().InSingletonScope();
+            Bind<AppConfigManager>().ToSelf().InSingletonScope();
+            Bind<UserConfigManager>().ToSelf().InSingletonScope();
+
+            // Update
+            Bind<IAutoUpdater>().To<AutoUpdater>();
+            Bind<ICurrentVersionSource>().To<CurrentVersionSource>();
+            Bind<ILatestVersionSource>().To<LatestVersionSource>();
+            Bind<IUrlFileDownloader>().To<UrlFileDownloader>();
+            Bind<IUrlStreamReader>().To<UrlStreamReader>();
+            Bind<IUrlTextReader>().To<UrlTextReader>();
+#if STANDALONE
+            Bind<Edition>().ToConstant(Edition.StandaloneZIP); // TODO: Match against all 4 editions
+#else
+            Bind<Edition>().ToConstant(Edition.InstallerEXE);
+#endif
+
+
+            // Misc
+            Bind<Logger>().ToMethod(ctx => LoggerFactory.Current.GetLogger()).InSingletonScope();
         }
     }
 }
