d9b8a8132035ae74569231098a85767de30369f9
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2015-05-07 13:42:04 -0400

    Make FRecover localizable

--- NAPS2.Core/WinForms/FRecover.Designer.cs
+++ NAPS2.Core/WinForms/FRecover.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FRecover));
             this.lblPrompt = new System.Windows.Forms.Label();
             this.btnRecover = new System.Windows.Forms.Button();
             this.btnDelete = new System.Windows.Forms.Button();
@@ -36,56 +37,40 @@
             // 
             // lblPrompt
             // 
-            this.lblPrompt.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblPrompt.Location = new System.Drawing.Point(12, 9);
+            resources.ApplyResources(this.lblPrompt, "lblPrompt");
             this.lblPrompt.Name = "lblPrompt";
-            this.lblPrompt.Size = new System.Drawing.Size(372, 46);
-            this.lblPrompt.TabIndex = 0;
-            this.lblPrompt.Text = "{0} image(s) scanned on {1} at {2} may not have been saved, and are recoverable. " +
-    "Do you want to recover them?";
             // 
             // btnRecover
             // 
-            this.btnRecover.Location = new System.Drawing.Point(12, 58);
+            resources.ApplyResources(this.btnRecover, "btnRecover");
             this.btnRecover.Name = "btnRecover";
-            this.btnRecover.Size = new System.Drawing.Size(120, 30);
-            this.btnRecover.TabIndex = 1;
-            this.btnRecover.Text = "&Recover";
             this.btnRecover.UseVisualStyleBackColor = true;
             this.btnRecover.Click += new System.EventHandler(this.btnRecover_Click);
             // 
             // btnDelete
             // 
-            this.btnDelete.Location = new System.Drawing.Point(138, 58);
+            resources.ApplyResources(this.btnDelete, "btnDelete");
             this.btnDelete.Name = "btnDelete";
-            this.btnDelete.Size = new System.Drawing.Size(120, 30);
-            this.btnDelete.TabIndex = 2;
-            this.btnDelete.Text = "&Delete";
             this.btnDelete.UseVisualStyleBackColor = true;
             this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
             // 
             // btnCancel
             // 
-            this.btnCancel.Location = new System.Drawing.Point(264, 58);
+            resources.ApplyResources(this.btnCancel, "btnCancel");
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(120, 30);
-            this.btnCancel.TabIndex = 3;
-            this.btnCancel.Text = "&Not Now";
             this.btnCancel.UseVisualStyleBackColor = true;
             this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             // 
             // FRecover
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(396, 100);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnDelete);
             this.Controls.Add(this.btnRecover);
             this.Controls.Add(this.lblPrompt);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Name = "FRecover";
-            this.Text = "Recover Scanned Images";
             this.ResumeLayout(false);
 
         }
