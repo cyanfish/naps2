9b3e55a74212013c50cdac80b1f4a7c607bba7c3
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2015-07-13 16:15:48 -0400

    PUBLISH (4.0b1)

--- NAPS2/Properties/AssemblyInfo.cs
+++ NAPS2/Properties/AssemblyInfo.cs
@@ -52,5 +52,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.3.5.*")]
+[assembly: AssemblyVersion("4.0.1.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
