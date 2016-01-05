679b3a1cf7ba843de1e5bbb44965250e50c9ef4c
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2016-01-05 13:12:53 -0500

    Add legacy TWAIN implementation (currently unused)

---
+++ NAPS2.Core/Scan/Twain/Legacy/TwainApi.cs
@@ -0,0 +1,179 @@
+/*
+    NAPS2 (Not Another PDF Scanner 2)
+    http://sourceforge.net/projects/naps2/
+    
+    Copyright (C) 2009       Pavel Sorejs
+    Copyright (C) 2012       Michael Adams
+    Copyright (C) 2013       Peter De Leeuw
+    Copyright (C) 2012-2014  Ben Olden-Cooligan
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+*/
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using NAPS2.Scan.Exceptions;
+using NAPS2.Scan.Images;
+using NAPS2.Util;
+using NAPS2.WinForms;
+
+namespace NAPS2.Scan.Twain.Legacy
+{
+    internal static class TwainApi
+    {
+        public static ScanDevice SelectDeviceUI()
+        {
+            var tw = new Twain();
+            if (!tw.Init(Application.OpenForms[0].Handle))
+            {
+                throw new NoDevicesFoundException();
+            }
+            if (!tw.Select())
+            {
+                return null;
+            }
+            string name = tw.GetCurrentName();
+            if (name == null)
+            {
+                return null;
+            }
+            return new ScanDevice(name, name);
+        }
+
+        public static List<IScannedImage> Scan(ScanProfile settings, ScanDevice device, IWin32Window pForm, IFormFactory formFactory, IScannedImageFactory scannedImageFactory)
+        {
+            var tw = new Twain();
+            if (!tw.Init(pForm.Handle))
+            {
+                throw new DeviceNotFoundException();
+            }
+            if (!tw.SelectByName(device.ID))
+            {
+                throw new DeviceNotFoundException();
+            }
+            var form = formFactory.Create<FTwainGui>();
+            var mf = new TwainMessageFilter(settings, tw, form, scannedImageFactory);
+            form.ShowDialog(pForm);
+            return mf.Bitmaps;
+        }
+
+        private class TwainMessageFilter : IMessageFilter
+        {
+            private readonly ScanProfile settings;
+            private readonly Twain tw;
+            private readonly FTwainGui form;
+            private readonly IScannedImageFactory scannedImageFactory;
+
+            private bool activated;
+            private bool msgfilter;
+
+            public TwainMessageFilter(ScanProfile settings, Twain tw, FTwainGui form, IScannedImageFactory scannedImageFactory)
+            {
+                this.settings = settings;
+                this.tw = tw;
+                this.form = form;
+                this.scannedImageFactory = scannedImageFactory;
+                Bitmaps = new List<IScannedImage>();
+                form.Activated += FTwainGui_Activated;
+            }
+
+            public List<IScannedImage> Bitmaps { get; private set; }
+
+            public bool PreFilterMessage(ref Message m)
+            {
+                TwainCommand cmd = tw.PassMessage(ref m);
+                if (cmd == TwainCommand.Not)
+                    return false;
+
+                switch (cmd)
+                {
+                    case TwainCommand.CloseRequest:
+                        {
+                            EndingScan();
+                            tw.CloseSrc();
+                            form.Close();
+                            break;
+                        }
+                    case TwainCommand.CloseOk:
+                        {
+                            EndingScan();
+                            tw.CloseSrc();
+                            break;
+                        }
+                    case TwainCommand.DeviceEvent:
+                        {
+                            break;
+                        }
+                    case TwainCommand.TransferReady:
+                        {
+                            ArrayList pics = tw.TransferPictures();
+                            EndingScan();
+                            tw.CloseSrc();
+                            foreach (IntPtr img in pics)
+                            {
+                                int bitcount = 0;
+
+                                using (Bitmap bmp = DibUtils.BitmapFromDib(img, out bitcount))
+                                {
+                                    Bitmaps.Add(scannedImageFactory.Create(bmp, bitcount == 1 ? ScanBitDepth.BlackWhite : ScanBitDepth.C24Bit, settings.MaxQuality));
+                                }
+                            }
+                            form.Close();
+                            break;
+                        }
+                }
+
+                return true;
+            }
+            private void EndingScan()
+            {
+                if (msgfilter)
+                {
+                    Application.RemoveMessageFilter(this);
+                    msgfilter = false;
+                    form.Enabled = true;
+                    form.Activate();
+                }
+            }
+
+            private void FTwainGui_Activated(object sender, EventArgs e)
+            {
+                if (activated)
+                    return;
+                activated = true;
+                if (!msgfilter)
+                {
+                    form.Enabled = false;
+                    msgfilter = true;
+                    Application.AddMessageFilter(this);
+                }
+                try
+                {
+                    if (!tw.Acquire())
+                    {
+                        EndingScan();
+                        form.Close();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Log.ErrorException("An error occurred while interacting with TWAIN.", ex);
+                    EndingScan();
+                    form.Close();
+                }
+            }
+        }
+    }
+}
---
+++ NAPS2.Core/Scan/Twain/Legacy/TwainLib.cs
@@ -0,0 +1,348 @@
+/*
+    NAPS2 (Not Another PDF Scanner 2)
+    http://sourceforge.net/projects/naps2/
+    
+    Copyright (C) 2009       Pavel Sorejs
+    Copyright (C) 2012       Michael Adams
+    Copyright (C) 2013       Peter De Leeuw
+    Copyright (C) 2012-2014  Ben Olden-Cooligan
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+*/
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace NAPS2.Scan.Twain.Legacy
+{
+    internal enum TwainCommand
+    {
+        Not = -1,
+        Null = 0,
+        TransferReady = 1,
+        CloseRequest = 2,
+        CloseOk = 3,
+        DeviceEvent = 4
+    }
+
+    internal class Twain
+    {
+        private const short COUNTRY_USA = 1;
+        private const short LANGUAGE_USA = 13;
+        private readonly TwIdentity appid;
+        private readonly TwIdentity srcds;
+        private TwEvent evtmsg;
+        private IntPtr hwnd;
+        private WINMSG winmsg;
+
+        public Twain()
+        {
+            appid = new TwIdentity
+                {
+                    Id = IntPtr.Zero,
+                    Version =
+                        {
+                            MajorNum = 1,
+                            MinorNum = 1,
+                            Language = LANGUAGE_USA,
+                            Country = COUNTRY_USA,
+                            Info = "Hack 1"
+                        },
+                    ProtocolMajor = TwProtocol.MAJOR,
+                    ProtocolMinor = TwProtocol.MINOR,
+                    SupportedGroups = (int)(TwDG.Image | TwDG.Control),
+                    Manufacturer = "NETMaster",
+                    ProductFamily = "Freeware",
+                    ProductName = "Hack"
+                };
+
+            srcds = new TwIdentity { Id = IntPtr.Zero };
+
+            evtmsg.EventPtr = Marshal.AllocHGlobal(Marshal.SizeOf(winmsg));
+        }
+
+        public static int ScreenBitDepth
+        {
+            get
+            {
+                IntPtr screenDC = CreateDC("DISPLAY", null, null, IntPtr.Zero);
+                int bitDepth = GetDeviceCaps(screenDC, 12);
+                bitDepth *= GetDeviceCaps(screenDC, 14);
+                DeleteDC(screenDC);
+                return bitDepth;
+            }
+        }
+
+        ~Twain()
+        {
+            Marshal.FreeHGlobal(evtmsg.EventPtr);
+        }
+
+        public bool Init(IntPtr hwndp)
+        {
+            Finish();
+            TwReturnCode returnCode = DSMparent(appid, IntPtr.Zero, TwDG.Control, TwData.Parent, TwMessageCode.OpenDSM, ref hwndp);
+            if (returnCode == TwReturnCode.Success)
+            {
+                returnCode = DSMident(appid, IntPtr.Zero, TwDG.Control, TwData.Identity, TwMessageCode.GetFirst, srcds);
+                if (returnCode == TwReturnCode.Success)
+                {
+                    hwnd = hwndp;
+                    return true;
+                }
+                else
+                {
+                    returnCode = DSMparent(appid, IntPtr.Zero, TwDG.Control, TwData.Parent, TwMessageCode.CloseDSM, ref hwndp);
+                    return false;
+                }
+            }
+            return false;
+        }
+
+        public bool Select()
+        {
+            TwReturnCode returnCode;
+            CloseSrc();
+            if (appid.Id == IntPtr.Zero)
+            {
+                Init(hwnd);
+                if (appid.Id == IntPtr.Zero)
+                    return false;
+            }
+            returnCode = DSMident(appid, IntPtr.Zero, TwDG.Control, TwData.Identity, TwMessageCode.UserSelect, srcds);
+            return returnCode == TwReturnCode.Success;
+        }
+
+        public bool SelectByName(string name)
+        {
+            if (srcds.ProductName == name)
+            {
+                return true;
+            }
+            var rc = TwReturnCode.Success;
+            while (rc == TwReturnCode.Success)
+            {
+                rc = DSMident(appid, IntPtr.Zero, TwDG.Control, TwData.Identity, TwMessageCode.GetNext, srcds);
+                if (srcds.ProductName == name)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public string GetCurrentName()
+        {
+            return srcds.ProductName;
+        }
+
+        public bool Acquire()
+        {
+            TwReturnCode returnCode;
+            CloseSrc();
+            if (appid.Id == IntPtr.Zero)
+            {
+                Init(hwnd);
+                if (appid.Id == IntPtr.Zero)
+                    throw new InvalidOperationException("Init call falied");
+            }
+            returnCode = DSMident(appid, IntPtr.Zero, TwDG.Control, TwData.Identity, TwMessageCode.OpenDS, srcds);
+            if (returnCode != TwReturnCode.Success)
+                throw new InvalidOperationException("DSMident call falied");
+
+            var guif = new TwUserInterface
+            {
+                ShowUI = 1,
+                ModalUI = 1,
+                ParentHand = hwnd
+            };
+            returnCode = DSuserif(appid, srcds, TwDG.Control, TwData.UserInterface, TwMessageCode.EnableDS, guif);
+            if (returnCode != TwReturnCode.Success)
+            {
+                CloseSrc();
+                if (returnCode == TwReturnCode.Cancel)
+                {
+                    return false;
+                }
+                throw new InvalidOperationException("DSuserif call falied");
+            }
+            return true;
+        }
+
+        public ArrayList TransferPictures()
+        {
+            var pics = new ArrayList();
+            if (srcds.Id == IntPtr.Zero)
+                return pics;
+
+            TwReturnCode returnCode;
+            IntPtr hbitmap = IntPtr.Zero;
+            var pxfr = new TwPendingXfers();
+
+            do
+            {
+                pxfr.Count = 0;
+                hbitmap = IntPtr.Zero;
+
+                var iinf = new TwImageInfo();
+                returnCode = DSiinf(appid, srcds, TwDG.Image, TwData.ImageInfo, TwMessageCode.Get, iinf);
+                if (returnCode != TwReturnCode.Success)
+                {
+                    CloseSrc();
+                    return pics;
+                }
+
+                returnCode = DSixfer(appid, srcds, TwDG.Image, TwData.ImageNativeXfer, TwMessageCode.Get, ref hbitmap);
+                if (returnCode != TwReturnCode.XferDone)
+                {
+                    CloseSrc();
+                    return pics;
+                }
+
+                returnCode = DSpxfer(appid, srcds, TwDG.Control, TwData.PendingXfers, TwMessageCode.EndXfer, pxfr);
+                if (returnCode != TwReturnCode.Success)
+                {
+                    CloseSrc();
+                    return pics;
+                }
+
+                pics.Add(hbitmap);
+            }
+            while (pxfr.Count != 0);
+
+            returnCode = DSpxfer(appid, srcds, TwDG.Control, TwData.PendingXfers, TwMessageCode.Reset, pxfr);
+            return pics;
+        }
+
+        public TwainCommand PassMessage(ref Message m)
+        {
+            if (srcds.Id == IntPtr.Zero)
+                return TwainCommand.Not;
+
+            int pos = GetMessagePos();
+
+            winmsg.hwnd = m.HWnd;
+            winmsg.message = m.Msg;
+            winmsg.wParam = m.WParam;
+            winmsg.lParam = m.LParam;
+            winmsg.time = GetMessageTime();
+            winmsg.x = (short)pos;
+            winmsg.y = (short)(pos >> 16);
+
+            Marshal.StructureToPtr(winmsg, evtmsg.EventPtr, false);
+            evtmsg.Message = 0;
+            TwReturnCode returnCode = DSevent(appid, srcds, TwDG.Control, TwData.Event, TwMessageCode.ProcessEvent, ref evtmsg);
+            if (returnCode == TwReturnCode.NotDSEvent)
+                return TwainCommand.Not;
+            if (evtmsg.Message == (short)TwMessageCode.XFerReady)
+                return TwainCommand.TransferReady;
+            if (evtmsg.Message == (short)TwMessageCode.CloseDSReq)
+                return TwainCommand.CloseRequest;
+            if (evtmsg.Message == (short)TwMessageCode.CloseDSOK)
+                return TwainCommand.CloseOk;
+            if (evtmsg.Message == (short)TwMessageCode.DeviceEvent)
+                return TwainCommand.DeviceEvent;
+
+            return TwainCommand.Null;
+        }
+
+        public void CloseSrc()
+        {
+            TwReturnCode returnCode;
+            if (srcds.Id != IntPtr.Zero)
+            {
+                var guif = new TwUserInterface();
+                returnCode = DSuserif(appid, srcds, TwDG.Control, TwData.UserInterface, TwMessageCode.DisableDS, guif);
+                returnCode = DSMident(appid, IntPtr.Zero, TwDG.Control, TwData.Identity, TwMessageCode.CloseDS, srcds);
+            }
+        }
+
+        public void Finish()
+        {
+            TwReturnCode returnCode;
+            CloseSrc();
+            if (appid.Id != IntPtr.Zero)
+                returnCode = DSMparent(appid, IntPtr.Zero, TwDG.Control, TwData.Parent, TwMessageCode.CloseDSM, ref hwnd);
+            appid.Id = IntPtr.Zero;
+        }
+
+        // ------ DSM entry point DAT_ variants:
+        [DllImport("twain_32.dll", EntryPoint = "#1")]
+        private static extern TwReturnCode DSMparent([In, Out] TwIdentity origin, IntPtr zeroptr, TwDG dg, TwData data, TwMessageCode messageCode, ref IntPtr refptr);
+
+        [DllImport("twain_32.dll", EntryPoint = "#1")]
+        private static extern TwReturnCode DSMident([In, Out] TwIdentity origin, IntPtr zeroptr, TwDG dg, TwData data, TwMessageCode messageCode, [In, Out] TwIdentity idds);
+
+        [DllImport("twain_32.dll", EntryPoint = "#1")]
+        private static extern TwReturnCode DSMstatus([In, Out] TwIdentity origin, IntPtr zeroptr, TwDG dg, TwData data, TwMessageCode messageCode, [In, Out] TwStatus dsmstat);
+
+        // ------ DSM entry point DAT_ variants to DS:
+        [DllImport("twain_32.dll", EntryPoint = "#1")]
+        private static extern TwReturnCode DSuserif([In, Out] TwIdentity origin, [In, Out] TwIdentity dest, TwDG dg, TwData data, TwMessageCode messageCode, TwUserInterface guif);
+
+        [DllImport("twain_32.dll", EntryPoint = "#1")]
+        private static extern TwReturnCode DSevent([In, Out] TwIdentity origin, [In, Out] TwIdentity dest, TwDG dg, TwData data, TwMessageCode messageCode, ref TwEvent evt);
+
+        [DllImport("twain_32.dll", EntryPoint = "#1")]
+        private static extern TwReturnCode DSstatus([In, Out] TwIdentity origin, [In] TwIdentity dest, TwDG dg, TwData data, TwMessageCode messageCode, [In, Out] TwStatus dsmstat);
+
+        [DllImport("twain_32.dll", EntryPoint = "#1")]
+        private static extern TwReturnCode DScap([In, Out] TwIdentity origin, [In] TwIdentity dest, TwDG dg, TwData data, TwMessageCode messageCode, [In, Out] TwCapability capa);
+
+        [DllImport("twain_32.dll", EntryPoint = "#1")]
+        private static extern TwReturnCode DSiinf([In, Out] TwIdentity origin, [In] TwIdentity dest, TwDG dg, TwData data, TwMessageCode messageCode, [In, Out] TwImageInfo imginf);
+
+        [DllImport("twain_32.dll", EntryPoint = "#1")]
+        private static extern TwReturnCode DSixfer([In, Out] TwIdentity origin, [In] TwIdentity dest, TwDG dg, TwData data, TwMessageCode messageCode, ref IntPtr hbitmap);
+
+        [DllImport("twain_32.dll", EntryPoint = "#1")]
+        private static extern TwReturnCode DSpxfer([In, Out] TwIdentity origin, [In] TwIdentity dest, TwDG dg, TwData data, TwMessageCode messageCode, [In, Out] TwPendingXfers pxfr);
+
+        [DllImport("kernel32.dll", ExactSpelling = true)]
+        internal static extern IntPtr GlobalAlloc(int flags, int size);
+        [DllImport("kernel32.dll", ExactSpelling = true)]
+        internal static extern IntPtr GlobalLock(IntPtr handle);
+        [DllImport("kernel32.dll", ExactSpelling = true)]
+        internal static extern bool GlobalUnlock(IntPtr handle);
+        [DllImport("kernel32.dll", ExactSpelling = true)]
+        internal static extern IntPtr GlobalFree(IntPtr handle);
+
+        [DllImport("user32.dll", ExactSpelling = true)]
+        private static extern int GetMessagePos();
+        [DllImport("user32.dll", ExactSpelling = true)]
+        private static extern int GetMessageTime();
+
+        [DllImport("gdi32.dll", ExactSpelling = true)]
+        private static extern int GetDeviceCaps(IntPtr hDC, int nIndex);
+
+        [DllImport("gdi32.dll", CharSet = CharSet.Auto)]
+        private static extern IntPtr CreateDC(string szdriver, string szdevice, string szoutput, IntPtr devmode);
+
+        [DllImport("gdi32.dll", ExactSpelling = true)]
+        private static extern bool DeleteDC(IntPtr hdc);
+
+        [StructLayout(LayoutKind.Sequential, Pack = 4)]
+        internal struct WINMSG
+        {
+            public IntPtr hwnd;
+            public int message;
+            public IntPtr wParam;
+            public IntPtr lParam;
+            public int time;
+            public int x;
+            public int y;
+        }
+    } // class Twain
+}
