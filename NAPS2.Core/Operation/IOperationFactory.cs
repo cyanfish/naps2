29d9e2b9bb1e04090c2bc3b9c500ef984b49fd8f
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2016-01-06 12:44:32 -0500

    Add OperationFactory

---
+++ NAPS2.Core/Operation/IOperationFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NAPS2.Operation
+{
+    public interface IOperationFactory
+    {
+        T Create<T>() where T : IOperation;
+    }
+}
---
+++ NAPS2/NinjectOperationFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NAPS2.Operation;
+using Ninject;
+
+namespace NAPS2
+{
+    public class NinjectOperationFactory : IOperationFactory
+    {
+        private readonly IKernel kernel;
+
+        public NinjectOperationFactory(IKernel kernel)
+        {
+            this.kernel = kernel;
+        }
+
+        public T Create<T>() where T : IOperation
+        {
+            var form = kernel.Get<T>();
+            return form;
+        }
+    }
+}
