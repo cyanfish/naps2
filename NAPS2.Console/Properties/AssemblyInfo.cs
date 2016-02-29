286df3320eec7610e4876cb4230400de22189d30
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2016-02-29 16:02:46 -0500

    PUBLISH (4.7.0)

--- NAPS2.Console/Properties/AssemblyInfo.cs
+++ NAPS2.Console/Properties/AssemblyInfo.cs
@@ -48,4 +48,4 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("4.6.1.*")]
+[assembly: AssemblyVersion("4.7.0.*")]
--- NAPS2/Properties/AssemblyInfo.cs
+++ NAPS2/Properties/AssemblyInfo.cs
@@ -52,4 +52,4 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("4.6.1.*")]
+[assembly: AssemblyVersion("4.7.0.*")]
