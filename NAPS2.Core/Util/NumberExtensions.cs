c4ed20bc1e5fc6eff7a592f51391f5daf1e04eee
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2017-05-13 16:45:57 -0400

    Add "Clamp" extension method

---
+++ NAPS2.Core/Util/NumberExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NAPS2.Util
+{
+    public static class NumberExtensions
+    {
+        public static T Clamp<T>(this T val, T min, T max) where T : IComparable<T>
+        {
+            if (val.CompareTo(min) < 0)
+            {
+                return min;
+            }
+            if (val.CompareTo(max) > 0)
+            {
+                return max;
+            }
+            return val;
+        }
+    }
+}
