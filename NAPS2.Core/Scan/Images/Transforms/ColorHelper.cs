2acf9829e2e651733af3973990506ab84787ab2b
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2017-11-11 23:48:44 -0500

    SaturationTransform implementation

---
+++ NAPS2.Core/Scan/Images/Transforms/ColorHelper.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace NAPS2.Scan.Images.Transforms
+{
+    public static class ColorHelper
+    {
+        // From https://stackoverflow.com/a/1626175
+
+        public static void ColorToHSV(Color color, out double hue, out double saturation, out double value)
+        {
+            int max = Math.Max(color.R, Math.Max(color.G, color.B));
+            int min = Math.Min(color.R, Math.Min(color.G, color.B));
+
+            hue = color.GetHue();
+            saturation = (max == 0) ? 0 : 1d - (1d * min / max);
+            value = max / 255d;
+        }
+
+        public static Color ColorFromHSV(double hue, double saturation, double value)
+        {
+            int hi = Convert.ToInt32(Math.Floor(hue / 60)) % 6;
+            double f = hue / 60 - Math.Floor(hue / 60);
+
+            value = value * 255;
+            int v = Convert.ToInt32(value);
+            int p = Convert.ToInt32(value * (1 - saturation));
+            int q = Convert.ToInt32(value * (1 - f * saturation));
+            int t = Convert.ToInt32(value * (1 - (1 - f) * saturation));
+
+            if (hi == 0)
+                return Color.FromArgb(255, v, t, p);
+            else if (hi == 1)
+                return Color.FromArgb(255, q, v, p);
+            else if (hi == 2)
+                return Color.FromArgb(255, p, v, t);
+            else if (hi == 3)
+                return Color.FromArgb(255, p, q, v);
+            else if (hi == 4)
+                return Color.FromArgb(255, t, p, v);
+            else
+                return Color.FromArgb(255, v, p, q);
+        }
+
+        public static void ColorToHSL(Color color, out double hue, out double saturation, out double brightness)
+        {
+            hue = color.GetHue();
+            saturation = color.GetSaturation();
+            brightness = color.GetBrightness();
+        }
+
+        // From https://blogs.msdn.microsoft.com/cjacks/2006/04/12/converting-from-hsb-to-rgb-in-net/
+        public static Color ColorFromHSL(double h, double s, double b)
+        {
+            int a = 255;
+
+            if (s == 0)
+            {
+                return Color.FromArgb(a, Convert.ToInt32(b * 255),
+                    Convert.ToInt32(b * 255), Convert.ToInt32(b * 255));
+            }
+
+            double fMax, fMid, fMin;
+            int iSextant, iMax, iMid, iMin;
+
+            if (0.5 < b)
+            {
+                fMax = b - (b * s) + s;
+                fMin = b + (b * s) - s;
+            }
+            else
+            {
+                fMax = b + (b * s);
+                fMin = b - (b * s);
+            }
+
+            iSextant = (int)Math.Floor(h / 60f);
+            if (300f <= h)
+            {
+                h -= 360f;
+            }
+            h /= 60f;
+            h -= 2f * (double)Math.Floor(((iSextant + 1f) % 6f) / 2f);
+            if (0 == iSextant % 2)
+            {
+                fMid = h * (fMax - fMin) + fMin;
+            }
+            else
+            {
+                fMid = fMin - h * (fMax - fMin);
+            }
+
+            iMax = Convert.ToInt32(fMax * 255);
+            iMid = Convert.ToInt32(fMid * 255);
+            iMin = Convert.ToInt32(fMin * 255);
+
+            switch (iSextant)
+            {
+                case 1:
+                    return Color.FromArgb(a, iMid, iMax, iMin);
+                case 2:
+                    return Color.FromArgb(a, iMin, iMax, iMid);
+                case 3:
+                    return Color.FromArgb(a, iMin, iMid, iMax);
+                case 4:
+                    return Color.FromArgb(a, iMid, iMin, iMax);
+                case 5:
+                    return Color.FromArgb(a, iMax, iMin, iMid);
+                default:
+                    return Color.FromArgb(a, iMax, iMid, iMin);
+            }
+        }
+    }
+}
--- NAPS2.Core/Scan/Images/Transforms/SaturationTransform.cs
+++ NAPS2.Core/Scan/Images/Transforms/SaturationTransform.cs
@@ -14,10 +14,35 @@ namespace NAPS2.Scan.Images.Transforms
 
         public override Bitmap Perform(Bitmap bitmap)
         {
-            double saturationAdjusted = Saturation / 1000.0;
+            double saturationAdjusted = Saturation / 1000.0 + 1;
 
             EnsurePixelFormat(ref bitmap);
-            
+
+            var data = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height), ImageLockMode.ReadWrite, bitmap.PixelFormat);
+            var stride = Math.Abs(data.Stride);
+            for (int y = 0; y < data.Height; y++)
+            {
+                for (int x = 0; x < data.Width; x++)
+                {
+                    int r = Marshal.ReadByte(data.Scan0 + stride * y + x * 3);
+                    int g = Marshal.ReadByte(data.Scan0 + stride * y + x * 3 + 1);
+                    int b = Marshal.ReadByte(data.Scan0 + stride * y + x * 3 + 2);
+
+                    Color c = Color.FromArgb(255, r, g, b);
+                    double h, s, v;
+                    ColorHelper.ColorToHSL(c, out h, out s, out v);
+
+                    s = Math.Min(s * saturationAdjusted, 1);
+
+                    c = ColorHelper.ColorFromHSL(h, s, v);
+
+                    Marshal.WriteByte(data.Scan0 + stride * y + x * 3, c.R);
+                    Marshal.WriteByte(data.Scan0 + stride * y + x * 3 + 1, c.G);
+                    Marshal.WriteByte(data.Scan0 + stride * y + x * 3 + 2, c.B);
+                }
+            }
+            bitmap.UnlockBits(data);
+
             return bitmap;
         }
 
