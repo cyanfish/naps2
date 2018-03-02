b0dc26c5077eb4353bc2ab8ce4e9f31eb2b94606
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2018-03-02 16:53:47 -0500

    Use multiple host processes

--- NAPS2.Core/Host/X86HostManager.cs
+++ NAPS2.Core/Host/X86HostManager.cs
@@ -15,58 +15,44 @@ namespace NAPS2.Host
         public const string HOST_EXE_NAME = "NAPS2.exe";
         public const string HOST_ARG = "/32BitHost";
 
-        private static readonly Lazy<ChannelFactory<IX86HostService>> ChannelFactory = new Lazy<ChannelFactory<IX86HostService>>(
-                () => new ChannelFactory<IX86HostService>(new NetNamedPipeBinding { SendTimeout = TimeSpan.FromHours(24) }, new EndpointAddress(PipeName)));
-        
-        private static Process _hostProcess;
-
-        public static string PipeName { get; set; }
-
-        public static void StartHostProcess()
+        public static Process StartHostProcess()
         {
-            if (!Environment.Is64BitProcess || _hostProcess != null)
-            {
-                return;
-            }
             var dir = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
             var hostProcessPath = Path.Combine(dir, HOST_EXE_NAME);
-            _hostProcess = Process.Start(new ProcessStartInfo {
+            var proc = Process.Start(new ProcessStartInfo {
                 FileName = hostProcessPath,
                 Arguments = HOST_ARG,
                 RedirectStandardOutput = true,
                 UseShellExecute = false
             });
-            if (_hostProcess != null)
+            if (proc == null)
             {
-                PipeName = string.Format(PIPE_NAME_FORMAT, _hostProcess.Id);
-            }
-            else
-            {
-                Log.Error("Could not start 32-bit host process");
-                return;
+                throw new Exception("Could not start 32-bit host process");
             }
 
             try
             {
                 var job = new Job();
-                job.AddProcess(_hostProcess.Handle);
+                job.AddProcess(proc.Handle);
             }
             catch
             {
-                _hostProcess.Kill();
+                proc.Kill();
                 throw;
             }
 
-            _hostProcess?.StandardOutput.Read();
+            proc.StandardOutput.Read();
+
+            return proc;
         }
 
         public static IX86HostService Connect()
         {
-            if (_hostProcess == null)
-            {
-                StartHostProcess();
-            }
-            return ChannelFactory.Value.CreateChannel();
+            var proc = StartHostProcess();
+            var pipeName = string.Format(PIPE_NAME_FORMAT, proc.Id);
+            var channelFactory = new ChannelFactory<IX86HostService>(new NetNamedPipeBinding {SendTimeout = TimeSpan.FromHours(24)},
+                new EndpointAddress(pipeName));
+            return channelFactory.CreateChannel();
         }
     }
 }
