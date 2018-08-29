06bb171b15ad27a7c738920c36f4001439562edf
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2018-08-29 12:35:14 -0400

    Fix path for version check

--- NAPS2/Setup/naps2.ps1
+++ NAPS2/Setup/naps2.ps1
@@ -12,7 +12,7 @@ function Get-Inno-Path {
 
 function Get-NAPS2-Version {
     & (Get-MSBuild-Path) ..\NAPS2.csproj /v:q /p:Configuration=Debug | Out-Null
-    $Version = [Reflection.AssemblyName]::GetAssemblyName([IO.Path]::Combine($pwd, "..\bin\Standalone\NAPS2.exe")).Version
+    $Version = [Reflection.AssemblyName]::GetAssemblyName([IO.Path]::Combine($pwd, "..\bin\Debug\NAPS2.exe")).Version
     $VersionStr = "" + $Version.Major + "." + $Version.Minor + "." + $Version.Build
     $VersionStr
 }
