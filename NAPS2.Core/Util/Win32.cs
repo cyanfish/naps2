770c9f186539c6cd34900b998d624bc50c04cdf1
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2018-09-08 22:55:35 -0400

    Add Win32 methods for scrolling

--- NAPS2.Core/Util/Win32.cs
+++ NAPS2.Core/Util/Win32.cs
@@ -13,6 +13,12 @@ namespace NAPS2.Util
         [DllImport("user32.dll", CharSet = CharSet.Auto)]
         public static extern int SendMessage(IntPtr hWnd, int wMsg, IntPtr wParam, IntPtr lParam);
 
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern int GetScrollPos(IntPtr hWnd, System.Windows.Forms.Orientation nBar);
+
+        [DllImport("user32.dll")]
+        public static extern int SetScrollPos(IntPtr hWnd, System.Windows.Forms.Orientation nBar, int nPos, bool bRedraw);
+
         [DllImport("user32.dll")]
         [return: MarshalAs(UnmanagedType.Bool)]
         public static extern bool SetForegroundWindow(IntPtr hWnd);
