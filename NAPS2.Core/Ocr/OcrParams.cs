155b874c71b9cdf38afbd6c4249a4452f840d86d
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2018-09-11 11:12:04 -0400

    Equatable OcrParams

--- NAPS2.Core/Ocr/OcrParams.cs
+++ NAPS2.Core/Ocr/OcrParams.cs
@@ -4,7 +4,7 @@ using System.Linq;
 
 namespace NAPS2.Ocr
 {
-    public class OcrParams
+    public class OcrParams : IEquatable<OcrParams>
     {
         public OcrParams()
         {
@@ -19,5 +19,32 @@ namespace NAPS2.Ocr
         public string LanguageCode { get; set; }
 
         public OcrMode Mode { get; set; }
+
+        public bool Equals(OcrParams other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return string.Equals(LanguageCode, other.LanguageCode) && Mode == other.Mode;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((OcrParams) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return ((LanguageCode != null ? LanguageCode.GetHashCode() : 0) * 397) ^ (int) Mode;
+            }
+        }
+
+        public static bool operator ==(OcrParams left, OcrParams right) => Equals(left, right);
+
+        public static bool operator !=(OcrParams left, OcrParams right) => !Equals(left, right);
     }
 }
