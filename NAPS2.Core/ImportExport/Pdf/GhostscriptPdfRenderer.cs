ff44ad2f7079c33b3af8c37ff99e6aceeaca9609
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2018-09-27 14:12:56 -0400

    Fix PDF rendering for absurdly large page sizes

--- NAPS2.Core/ImportExport/Pdf/GhostscriptPdfRenderer.cs
+++ NAPS2.Core/ImportExport/Pdf/GhostscriptPdfRenderer.cs
@@ -9,6 +9,8 @@ using NAPS2.Dependencies;
 using NAPS2.Lang.Resources;
 using NAPS2.Scan;
 using NAPS2.Util;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
 
 namespace NAPS2.ImportExport.Pdf
 {
@@ -45,6 +47,13 @@ namespace NAPS2.ImportExport.Pdf
 
             // TODO: Maybe allow this to be configured
             int dpi = ScanDpi.Dpi300.ToIntDpi();
+
+            using (var doc = PdfReader.Open(path, PdfDocumentOpenMode.InformationOnly))
+            {
+                // Cap the resolution to 10k pixels in each dimension
+                dpi = Math.Min(dpi, (int) (10000 / doc.Pages[0].Height.Inch));
+                dpi = Math.Min(dpi, (int) (10000 / doc.Pages[0].Width.Inch));
+            }
 
             using (var stream = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read))
             {
