47469202bcbdfa3369f53a5f7c1e59f890811168
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2018-11-07 15:16:34 -0500

    CppWia WIP

--- NAPS2.Core/Scan/Wia/CppWia.cs
+++ NAPS2.Core/Scan/Wia/CppWia.cs
@@ -8,27 +8,27 @@ namespace NAPS2.Scan.Wia
     public static class CppWia
     {
         [DllImport("NAPS2.WIA.dll")]
-        public static extern int GetDeviceManager([Out] out IntPtr deviceManager);
+        public static extern uint GetDeviceManager([Out] out IntPtr deviceManager);
 
         [DllImport("NAPS2.WIA.dll")]
-        public static extern int GetDevice(IntPtr deviceManager, [MarshalAs(UnmanagedType.BStr)] string deviceId, [Out] out IntPtr device);
+        public static extern uint GetDevice(IntPtr deviceManager, [MarshalAs(UnmanagedType.BStr)] string deviceId, [Out] out IntPtr device);
 
         [DllImport("NAPS2.WIA.dll")]
-        public static extern int SetDeviceProperty(IntPtr device, int propId, int value);
+        public static extern uint SetDeviceProperty(IntPtr device, int propId, int value);
 
         [DllImport("NAPS2.WIA.dll")]
-        public static extern int GetItem(IntPtr device, [MarshalAs(UnmanagedType.BStr)] string itemId, [Out] out IntPtr item);
+        public static extern uint GetItem(IntPtr device, [MarshalAs(UnmanagedType.BStr)] string itemId, [Out] out IntPtr item);
 
         [DllImport("NAPS2.WIA.dll")]
-        public static extern int SetItemProperty(IntPtr item, int propId, int value);
+        public static extern uint SetItemProperty(IntPtr item, int propId, int value);
 
         [DllImport("NAPS2.WIA.dll")]
-        public static extern int StartTransfer(IntPtr item, [Out] out IntPtr transfer);
+        public static extern uint StartTransfer(IntPtr item, [Out] out IntPtr transfer);
 
         [DllImport("NAPS2.WIA.dll")]
-        public static extern int Download(IntPtr transfer, int flags, IntPtr callbackTodo, [Out] out byte[] bytes);
+        public static extern uint Download(IntPtr transfer, int flags, IntPtr callbackTodo, [Out] out byte[] bytes);
 
         [DllImport("NAPS2.WIA.dll")]
-        public static extern int EndTransfer(IntPtr transfer);
+        public static extern uint EndTransfer(IntPtr transfer);
     }
 }
