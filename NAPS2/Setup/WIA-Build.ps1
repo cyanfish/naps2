34de6d22807f70d50b4e35206d0da076fe0463bc
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2018-11-24 15:10:19 -0500

    WIA build events/scripts

---
+++ NAPS2/Setup/WIA-Build.ps1
@@ -0,0 +1,12 @@
+﻿param()
+
+. .\naps2.ps1
+
+$msbuild = Get-MSBuild-Path
+"Building NAPS2.WIA in Debug mode"
+& $msbuild ..\..\NAPS2.WIA\NAPS2.WIA.vcxproj /v:q /p:"Configuration=Debug;Platform=Win32"
+& $msbuild ..\..\NAPS2.WIA\NAPS2.WIA.vcxproj /v:q /p:"Configuration=Debug;Platform=x64"
+"Building NAPS2.WIA in Release mode"
+& $msbuild ..\..\NAPS2.WIA\NAPS2.WIA.vcxproj /v:q /p:"Configuration=Release;Platform=Win32"
+& $msbuild ..\..\NAPS2.WIA\NAPS2.WIA.vcxproj /v:q /p:"Configuration=Release;Platform=x64"
+"Done"
