1ce536193ebb728a34223107b87a8325bea99d8f
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2018-12-07 13:16:28 -0500

    Add DebugTimer class

---
+++ NAPS2.Core/Util/DebugTimer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace NAPS2.Util
+{
+    public class DebugTimer : IDisposable
+    {
+        private readonly string label;
+        private readonly Stopwatch stopwatch;
+
+        public DebugTimer(string label = null)
+        {
+            this.label = label;
+            stopwatch = Stopwatch.StartNew();
+        }
+
+        public void Dispose()
+        {
+            Debug.WriteLine(label == null
+                ? $"{stopwatch.ElapsedMilliseconds} ms"
+                : $"{stopwatch.ElapsedMilliseconds} ms : {label}");
+        }
+    }
+}
