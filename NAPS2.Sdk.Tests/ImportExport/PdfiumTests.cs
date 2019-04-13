ded344fd12532adff49070c87b7810dcdbd6de23
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2019-04-13 17:46:28 -0400

    Add Pdfium test

---
+++ NAPS2.Sdk.Tests/ImportExport/PdfiumTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using NAPS2.ImportExport.Pdf;
+using Xunit;
+
+namespace NAPS2.Sdk.Tests.ImportExport
+{
+    public class PdfiumTests : ContextualTexts
+    {
+        [Fact]
+        public void RenderPdfFromWord()
+        {
+            var path = Path.Combine(FolderPath, "word.pdf");
+            File.WriteAllBytes(path, PdfiumTestsData.word);
+
+            var images = PdfiumPdfRenderer.Render(path, 300).ToList();
+
+            Assert.Equal(2, images.Count);
+            Assert.Equal(images[0].Width, (int)(8.5 * 300));
+            Assert.Equal(images[0].Height, 11 * 300);
+            Assert.Equal(images[0].HorizontalResolution, 300);
+            Assert.Equal(images[0].VerticalResolution, 300);
+        }
+    }
+}
