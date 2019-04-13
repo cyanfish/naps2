afb07f9b979900b3c879078ff3b506b2ed630cfd
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2019-04-13 17:53:45 -0400

    Use image comparison for Pdfium test

--- NAPS2.Sdk.Tests/ImportExport/PdfiumTests.cs
+++ NAPS2.Sdk.Tests/ImportExport/PdfiumTests.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using NAPS2.Images.Storage;
 using NAPS2.ImportExport.Pdf;
+using NAPS2.Sdk.Tests.Asserts;
 using Xunit;
 
 namespace NAPS2.Sdk.Tests.ImportExport
@@ -15,13 +17,14 @@ namespace NAPS2.Sdk.Tests.ImportExport
             var path = Path.Combine(FolderPath, "word.pdf");
             File.WriteAllBytes(path, PdfiumTestsData.word);
 
+            IImage expectedPage1 = new GdiImage(PdfiumTestsData.word_p1);
+            IImage expectedPage2 = new GdiImage(PdfiumTestsData.word_p2);
+
             var images = PdfiumPdfRenderer.Render(path, 300).ToList();
 
             Assert.Equal(2, images.Count);
-            Assert.Equal(images[0].Width, (int)(8.5 * 300));
-            Assert.Equal(images[0].Height, 11 * 300);
-            Assert.Equal(images[0].HorizontalResolution, 300);
-            Assert.Equal(images[0].VerticalResolution, 300);
+            ImageAsserts.Similar(images[0], expectedPage1, 3);
+            ImageAsserts.Similar(images[1], expectedPage2, 3);
         }
     }
 }
--- NAPS2.Sdk.Tests/ImportExport/PdfiumTestsData.Designer.cs
+++ NAPS2.Sdk.Tests/ImportExport/PdfiumTestsData.Designer.cs
@@ -69,5 +69,25 @@ namespace NAPS2.Sdk.Tests.ImportExport {
                 return ((byte[])(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap word_p1 {
+            get {
+                object obj = ResourceManager.GetObject("word_p1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap word_p2 {
+            get {
+                object obj = ResourceManager.GetObject("word_p2", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
