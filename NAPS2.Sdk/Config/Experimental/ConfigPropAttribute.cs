329e0250df29e8f58b4d68bb5b27ebbd7784f221
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2019-04-14 11:43:36 -0400

    Remove use of XmlElementAttribute.Order since it can break deserialization

--- NAPS2.Sdk/Config/Experimental/ConfigPropAttribute.cs
+++ NAPS2.Sdk/Config/Experimental/ConfigPropAttribute.cs
@@ -9,8 +9,15 @@ namespace NAPS2.Config.Experimental
     {
         protected ConfigPropAttribute(int line)
         {
-            Order = line;
+            // Ideally we would use XmlElementAttribute.Order so the generated XML files have a nice ordering.
+            // However, that can break deserialization in case the order changes (e.g. new properties).
+            // So we have to be satisfied with this, which can at least be used to generate appsettings.xml.
+            ConfigOrdering = line;
+            // Setting nullable ensures all properties are included in the output. Otherwise there is an
+            // inconsistency between reference types and nullable value types.
             IsNullable = true;
         }
+
+        public int ConfigOrdering { get; set; }
     }
 }
