d5928e5352cfb4d39b980c1f167d50c5a4808d1e
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2019-05-06 21:03:25 -0400

    Add deskew test

---
+++ NAPS2.Sdk.Tests/Images/DeskewTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NAPS2.Images;
+using NAPS2.Images.Storage;
+using Xunit;
+
+namespace NAPS2.Sdk.Tests.Images
+{
+    public class DeskewTests
+    {
+        // Just a sanity check...
+        // TODO: More thorough deskew performance testing.
+        [Fact]
+        public void Deskew()
+        {
+            var image = new GdiImage(DeskewTestsData.skewed);
+            var deskewer = new HoughLineDeskewer();
+            var skewAngle = deskewer.GetSkewAngle(image);
+            Assert.InRange(skewAngle, 15.5, 16.5);
+        }
+    }
+}
