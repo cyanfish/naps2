42ad5060dd3e10e6346247a8ba60a5063f27a7c9
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2019-05-06 21:52:55 -0400

    Start adding scan pipeline tests

---
+++ NAPS2.Sdk.Tests/Scan/RemoteScanControllerTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Moq;
+using NAPS2.Images;
+using NAPS2.Scan;
+using NAPS2.Scan.Experimental;
+using NAPS2.Scan.Experimental.Internal;
+using Xunit;
+using IScanDriver = NAPS2.Scan.Experimental.Internal.IScanDriver;
+using IScanDriverFactory = NAPS2.Scan.Experimental.Internal.IScanDriverFactory;
+
+namespace NAPS2.Sdk.Tests.Scan
+{
+    public class RemoteScanControllerTests
+    {
+        [Fact]
+        public void GetDeviceList()
+        {
+            var device = new ScanDevice("test_id1", "test_name1");
+            var wiaDevice = new ScanDevice("WIA-test_id2", "test_name2");
+            var scanDriver = new Mock<IScanDriver>();
+            scanDriver.Setup(x => x.GetDeviceList(It.IsAny<ScanOptions>())).Returns(new List<ScanDevice> { device, wiaDevice });
+            var scanDriverFactory = new Mock<IScanDriverFactory>();
+            scanDriverFactory.Setup(x => x.Create(It.IsAny<ScanOptions>())).Returns(scanDriver.Object);
+            var controller = new RemoteScanController(scanDriverFactory.Object, new RemotePostProcessor(new ThresholdBlankDetector()));
+
+            var deviceList = controller.GetDeviceList(new ScanOptions { Driver = Driver.Wia });
+            Assert.Equal(2, deviceList.Count);
+            Assert.Equal("test_id1", deviceList[0].ID);
+            Assert.Equal("WIA-test_id2", deviceList[1].ID);
+
+            deviceList = controller.GetDeviceList(new ScanOptions { Driver = Driver.Twain });
+            Assert.Single(deviceList);
+            Assert.Equal("test_id1", deviceList[0].ID);
+
+            deviceList = controller.GetDeviceList(new ScanOptions { Driver = Driver.Twain, TwainOptions = { IncludeWiaDevices = true } });
+            Assert.Equal(2, deviceList.Count);
+        }
+    }
+}
--- NAPS2.Sdk/Properties/AssemblyInfo.cs
+++ NAPS2.Sdk/Properties/AssemblyInfo.cs
@@ -15,6 +15,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 
 [assembly: InternalsVisibleTo("NAPS2.Sdk.Tests")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
