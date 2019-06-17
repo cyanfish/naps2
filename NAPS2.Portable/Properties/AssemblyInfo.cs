b4757350b42646a8c521409993d504f858230b51
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2019-06-16 20:53:52 -0400

    PUBLISH (6.1.1)

--- NAPS2.Portable/Properties/AssemblyInfo.cs
+++ NAPS2.Portable/Properties/AssemblyInfo.cs
@@ -28,4 +28,4 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("6.1.0.*")]
+[assembly: AssemblyVersion("6.1.1.*")]
--- NAPS2.Server/Properties/AssemblyInfo.cs
+++ NAPS2.Server/Properties/AssemblyInfo.cs
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("6.1.0.*")]
+[assembly: AssemblyVersion("6.1.1.*")]
