5a8176bb465e21209fd1abcebe2a3cfc309c9e54
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2019-07-14 23:05:15 -0400

    Clarify comment

--- NAPS2.Sdk/Serialization/SerializedImageHelper.cs
+++ NAPS2.Sdk/Serialization/SerializedImageHelper.cs
@@ -110,8 +110,9 @@ namespace NAPS2.Serialization
         public class DeserializeOptions
         {
             /// <summary>
-            /// If true, the deserializer guarantees that the file storage will not be used for longer than the duration of the RPC call.
-            /// In this way, files can be reused even if ownership isn't transferred to the callee.
+            /// If true, the Deserialize caller guarantees that the file storage will not be used for longer than the duration of the RPC call.
+            /// In this way, files can be safely reused even if ownership isn't transferred to the callee.
+            /// This should not be true outside of an RPC context.
             /// </summary>
             public bool ShareFileStorage { get; set; }
         }
