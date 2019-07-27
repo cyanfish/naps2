623f3b147bf8744f675aed661bfffdf5197dc0b0
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2019-07-27 19:26:03 -0400

    Add ListMutationTests class

---
+++ NAPS2.Sdk.Tests/Asserts/CollectionAsserts.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace NAPS2.Sdk.Tests.Asserts
+{
+    public static class CollectionAsserts
+    {
+        public static void SameItems<T>(IEnumerable<T> first, IEnumerable<T> second)
+        {
+            Assert.Equal(first.OrderBy(x => x), second.OrderBy(x => x));
+        }
+    }
+}
---
+++ NAPS2.Sdk.Tests/Util/ListMutationTests.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+using NAPS2.Images;
+using NAPS2.Sdk.Tests.Asserts;
+using Xunit;
+
+namespace NAPS2.Sdk.Tests.Util
+{
+    public class ListMutationTests
+    {
+        [Fact]
+        public void MoveDown()
+        {
+            var m = new ListMutation<string>.MoveDown();
+            var list = new List<string> { "a", "b", "c" };
+            var sel = ListSelection.Single("b");
+
+            m.Apply(list, ref sel);
+
+            Assert.Equal(list, new[] { "a", "c", "b" });
+            CollectionAsserts.SameItems(sel, new[] { "b" });
+        }
+
+        [Fact]
+        public void MoveDown_EndOfList()
+        {
+            var m = new ListMutation<string>.MoveDown();
+            var list = new List<string> { "a", "b", "c" };
+            var sel = ListSelection.Single("c");
+
+            m.Apply(list, ref sel);
+
+            Assert.Equal(list, new[] { "a", "b", "c" });
+            CollectionAsserts.SameItems(sel, new[] { "c" });
+        }
+
+        [Fact]
+        public void MoveDown_Multiple()
+        {
+            var m = new ListMutation<string>.MoveDown();
+            var list = new List<string> { "a", "b", "c", "d" };
+            var sel = ListSelection.From(new[] { "a", "c" });
+
+            m.Apply(list, ref sel);
+
+            Assert.Equal(list, new[] { "b", "a", "d", "c" });
+            CollectionAsserts.SameItems(sel, new[] { "a", "c" });
+        }
+    }
+}
