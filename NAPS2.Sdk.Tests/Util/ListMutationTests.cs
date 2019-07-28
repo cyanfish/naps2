0bbd5168ec3984e2bbf4be7654724556b2c2ec75
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2019-07-28 19:33:25 -0400

    ListMutation tests wip

--- NAPS2.Sdk.Tests/Util/ListMutationTests.cs
+++ NAPS2.Sdk.Tests/Util/ListMutationTests.cs
@@ -11,39 +11,68 @@ namespace NAPS2.Sdk.Tests.Util
         public void MoveDown()
         {
             var m = new ListMutation<string>.MoveDown();
-            var list = new List<string> { "a", "b", "c" };
-            var sel = ListSelection.Single("b");
-
-            m.Apply(list, ref sel);
-
-            Assert.Equal(list, new[] { "a", "c", "b" });
-            CollectionAsserts.SameItems(sel, new[] { "b" });
-        }
-
-        [Fact]
-        public void MoveDown_EndOfList()
-        {
-            var m = new ListMutation<string>.MoveDown();
-            var list = new List<string> { "a", "b", "c" };
-            var sel = ListSelection.Single("c");
-
-            m.Apply(list, ref sel);
-
-            Assert.Equal(list, new[] { "a", "b", "c" });
-            CollectionAsserts.SameItems(sel, new[] { "c" });
-        }
-
-        [Fact]
-        public void MoveDown_Multiple()
-        {
-            var m = new ListMutation<string>.MoveDown();
             var list = new List<string> { "a", "b", "c", "d" };
-            var sel = ListSelection.From(new[] { "a", "c" });
+            var sel = ListSelection.Of("c");
 
+            // Move single down
             m.Apply(list, ref sel);
+            Assert.Equal(list, new[] { "a", "b", "d", "c" });
+            CollectionAsserts.SameItems(sel, new[] { "c" });
 
-            Assert.Equal(list, new[] { "b", "a", "d", "c" });
-            CollectionAsserts.SameItems(sel, new[] { "a", "c" });
+            // End of list, don't do anything
+            m.Apply(list, ref sel);
+            Assert.Equal(list, new[] { "a", "b", "d", "c" });
+            CollectionAsserts.SameItems(sel, new[] { "c" });
+            
+            // Move multiple down
+            sel = ListSelection.Of("a", "d");
+            m.Apply(list, ref sel);
+            Assert.Equal(list, new[] { "b", "a", "c", "d" });
+            CollectionAsserts.SameItems(sel, new[] { "a", "d" });
+            
+            // Multiple down, one at end of list
+            m.Apply(list, ref sel);
+            Assert.Equal(list, new[] { "b", "c", "a", "d" });
+            CollectionAsserts.SameItems(sel, new[] { "a", "d" });
+            
+            // Multiple down, both at end of list
+            m.Apply(list, ref sel);
+            Assert.Equal(list, new[] { "b", "c", "a", "d" });
+            CollectionAsserts.SameItems(sel, new[] { "a", "d" });
+        }
+        
+        [Fact]
+        public void MoveUp()
+        {
+            var m = new ListMutation<string>.MoveUp();
+            var list = new List<string> { "a", "b", "c", "d" };
+            var sel = ListSelection.Of("b");
+
+            // Move single up
+            m.Apply(list, ref sel);
+            Assert.Equal(list, new[] { "b", "a", "c", "d" });
+            CollectionAsserts.SameItems(sel, new[] { "b" });
+
+            // End of list, don't do anything
+            m.Apply(list, ref sel);
+            Assert.Equal(list, new[] { "b", "a", "c", "d" });
+            CollectionAsserts.SameItems(sel, new[] { "b" });
+            
+            // Move multiple up
+            sel = ListSelection.Of("a", "d");
+            m.Apply(list, ref sel);
+            Assert.Equal(list, new[] { "a", "b", "d", "c" });
+            CollectionAsserts.SameItems(sel, new[] { "a", "d" });
+            
+            // Multiple up, one at end of list
+            m.Apply(list, ref sel);
+            Assert.Equal(list, new[] { "a", "d", "b", "c" });
+            CollectionAsserts.SameItems(sel, new[] { "a", "d" });
+            
+            // Multiple up, both at end of list
+            m.Apply(list, ref sel);
+            Assert.Equal(list, new[] { "a", "d", "b", "c" });
+            CollectionAsserts.SameItems(sel, new[] { "a", "d" });
         }
         
         // TODO: More
