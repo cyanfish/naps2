e3823d9ccffb053aac5a3f57d66043cec6e40b5e
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2019-07-28 11:32:02 -0400

    Remove outdated todo

--- NAPS2.Sdk/ImportExport/Email/EmailSetup.cs
+++ NAPS2.Sdk/ImportExport/Email/EmailSetup.cs
@@ -4,12 +4,6 @@ namespace NAPS2.ImportExport.Email
 {
     public class EmailSetup
     {
-        // TODO: Should more of this be encrypted? Probably just pw/token is fine.
-        // Also, how would this work securely from an appsettings pov?
-        // Remove the "Secure" suffix here. Then encrypt when mapping from appsettings.
-        // Consider some kind of prefix for the encrypted text so anyone looking at the xml knows it is encrypted. "secure-xxx" "encrypted:xxx"
-        // Would also allow an error when mapping from appsettings if they copy the wrong thing.
-        // Consider how to behave if ProviderType is set (e.g. to gmail) but the user/token isn't.
         public EmailProviderType? ProviderType { get; set; }
 
         public string SystemProviderName { get; set; }
