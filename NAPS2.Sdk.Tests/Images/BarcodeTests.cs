f4d6b28d2b4bafca0966677b38c33a7bd3ec4e65
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2020-02-08 21:39:57 -0500

    Add barcode unit tests

--- NAPS2.Sdk.Tests/Images/BarcodeTests.cs
+++ NAPS2.Sdk.Tests/Images/BarcodeTests.cs
@@ -1,9 +1,56 @@
+using NAPS2.Images.Storage;
+using NAPS2.Scan;
+using Xunit;
+
 namespace NAPS2.Sdk.Tests.Images
 {
     public class BarcodeTests
     {
-        // TODO: Add unit tests for Patch-T, UPC, and no barcode present
         // TODO: Also add unit/integration tests for scan/import to ensure things are set correctly (and don't malfunction if detection is off...)
         // TODO: Also add unit/integration tests for patch-t splitting for batch/commandline etc, to ensure detection is enabled and splitting works
+        
+        [Fact]
+        public void DetectPatchT()
+        {
+            var image = new GdiImage(BarcodeTestsData.patcht);
+            var detection = BarcodeDetection.Detect(image, new BarcodeDetectionOptions
+            {
+                DetectBarcodes = true,
+                PatchTOnly = true
+            });
+            Assert.True(detection.IsAttempted);
+            Assert.True(detection.IsBarcodePresent);
+            Assert.True(detection.IsPatchT);
+        }
+        
+        [Fact]
+        public void DetectUpc()
+        {
+            var image = new GdiImage(BarcodeTestsData.image_upc_barcode);
+            var detection = BarcodeDetection.Detect(image, new BarcodeDetectionOptions
+            {
+                DetectBarcodes = true,
+                PatchTOnly = false
+            });
+            Assert.True(detection.IsAttempted);
+            Assert.True(detection.IsBarcodePresent);
+            Assert.False(detection.IsPatchT);
+            Assert.Equal("725272730706", detection.DetectionResult.Text);
+        }
+        
+        [Fact]
+        public void DetectNothing()
+        {
+            var image = new GdiImage(BarcodeTestsData.color_image);
+            var detection = BarcodeDetection.Detect(image, new BarcodeDetectionOptions
+            {
+                DetectBarcodes = true,
+                PatchTOnly = false
+            });
+            Assert.True(detection.IsAttempted);
+            Assert.False(detection.IsBarcodePresent);
+            Assert.False(detection.IsPatchT);
+            Assert.Null(detection.DetectionResult);
+        }
     }
 }
