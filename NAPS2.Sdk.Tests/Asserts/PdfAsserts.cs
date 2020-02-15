552effe96a0aaccd74a7456b2166f8125c347361
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2020-02-14 23:39:42 -0500

    Reuse pdfavalidator instance

--- NAPS2.Sdk.Tests/Asserts/PdfAsserts.cs
+++ NAPS2.Sdk.Tests/Asserts/PdfAsserts.cs
@@ -1,10 +1,15 @@
-﻿using PdfSharp.Pdf.IO;
+﻿using System;
+using PdfSharp.Pdf.IO;
 using Xunit;
 
 namespace NAPS2.Sdk.Tests.Asserts
 {
+    using PdfAValidator = PdfAValidator.PdfAValidator;
+    
     public static class PdfAsserts
     {
+        private static readonly Lazy<PdfAValidator> LazyPdfAValidator = new Lazy<PdfAValidator>(() => new PdfAValidator());
+        
         public static void AssertPageCount(int count, string filePath)
         {
             var doc = PdfReader.Open(filePath, PdfDocumentOpenMode.InformationOnly);
@@ -13,8 +18,7 @@ namespace NAPS2.Sdk.Tests.Asserts
 
         public static void AssertCompliant(string profile, string filePath)
         {
-            using var validator = new PdfAValidator.PdfAValidator();
-            var report = validator.ValidateWithDetailedReport(filePath);
+            var report = LazyPdfAValidator.Value.ValidateWithDetailedReport(filePath);
             Assert.True(report.Jobs.Job.ValidationReport.IsCompliant);
             Assert.StartsWith($"{profile} ", report.Jobs.Job.ValidationReport.ProfileName);
         }
