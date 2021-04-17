0e0568a3ec723585b8f6f6f5c0826ff81a905996
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2021-04-16 19:52:00 -0700

    Add support for C# 9 records

---
+++ NAPS2.Sdk/Util/IsExternalInit.cs
@@ -0,0 +1,6 @@
+// https://sergiopedri.medium.com/enabling-and-using-c-9-features-on-older-and-unsupported-runtimes-ce384d8debb
+// ReSharper disable once CheckNamespace
+namespace System.Runtime.CompilerServices
+{
+    internal static class IsExternalInit {}
+}
