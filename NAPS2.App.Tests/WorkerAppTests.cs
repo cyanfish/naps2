4d9a8857006cfe78135945b8e9fb4fcce2f00c28
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2022-06-19 18:40:16 -0700

    Tester the worker app channel

--- NAPS2.App.Tests/WorkerAppTests.cs
+++ NAPS2.App.Tests/WorkerAppTests.cs
@@ -1,16 +1,22 @@
+using GrpcDotNetNamedPipes;
+using NAPS2.Remoting.Worker;
+using NAPS2.Sdk.Tests;
 using Xunit;
 
 namespace NAPS2.App.Tests;
 
-public class WorkerAppTests
+public class WorkerAppTests : ContextualTexts
 {
     [Fact]
-    public void CreatesWindow()
+    public void CreatesPipeServer()
     {
         var process = AppTestHelper.StartProcess("NAPS2.Worker.exe", Process.GetCurrentProcess().Id.ToString());
         try
         {
             Assert.Equal("ready", process.StandardOutput.ReadLine());
+            string pipeName = $"NAPS2.Worker/{process.Id}";
+            var client = new WorkerServiceAdapter(new NamedPipeChannel(".", pipeName));
+            client.Init(FolderPath);
         }
         finally
         {
