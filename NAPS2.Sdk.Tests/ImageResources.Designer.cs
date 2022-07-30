3f6c62d223d37ed31f907006e87a662b51be9455
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2022-07-30 10:37:12 -0700

    Add "huge" image file for test use

--- NAPS2.Sdk.Tests/ImageResources.Designer.cs
+++ NAPS2.Sdk.Tests/ImageResources.Designer.cs
@@ -203,6 +203,16 @@ namespace NAPS2.Sdk.Tests {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap color_image_huge {
+            get {
+                object obj = ResourceManager.GetObject("color_image_huge", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap color_image_low_quality {
             get {
                 object obj = ResourceManager.GetObject("color_image_low_quality", resourceCulture);
