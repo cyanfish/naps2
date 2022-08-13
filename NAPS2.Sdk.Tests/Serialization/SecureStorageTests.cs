3d9fea818609a50c59c7818daace7da39f194230
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2022-08-13 18:11:33 -0400

    Mac: Fix SecureStorageTests

--- NAPS2.Sdk.Tests/Serialization/SecureStorageTests.cs
+++ NAPS2.Sdk.Tests/Serialization/SecureStorageTests.cs
@@ -5,13 +5,24 @@ namespace NAPS2.Sdk.Tests.Serialization;
 
 public class SecureStorageTests
 {
-    [Fact]
+    [PlatformFact(include: Platform.Windows)]
     public void EncryptAndDecrypt()
     {
         var text = "Hello, world!";
         var encrypted = SecureStorage.Encrypt(text);
         Assert.StartsWith("encrypted-", encrypted);
         Assert.DoesNotContain("Hello", encrypted);
+
+        var decrypted = SecureStorage.Decrypt(encrypted);
+        Assert.Equal(text, decrypted);
+    }
+    
+    [PlatformFact(exclude: Platform.Windows)]
+    public void EncryptAndDecryptNonWindows()
+    {
+        var text = "Hello, world!";
+        var encrypted = SecureStorage.Encrypt(text);
+        Assert.Equal(text, encrypted);
 
         var decrypted = SecureStorage.Decrypt(encrypted);
         Assert.Equal(text, decrypted);
