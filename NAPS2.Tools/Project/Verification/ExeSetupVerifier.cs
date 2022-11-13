462c0689835a763b68b5dcd82f13f20713e176bf
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2022-11-13 11:57:18 -0800

    Add test for exe installer upgrade

--- NAPS2.Tools/Project/Verification/ExeSetupVerifier.cs
+++ NAPS2.Tools/Project/Verification/ExeSetupVerifier.cs
@@ -7,11 +7,54 @@ public static class ExeSetupVerifier
 {
     public static void Verify(Platform platform, string version, bool verbose)
     {
-        // TODO: Verify upgrades work correct (including for 32 -> 64 bit)
+        // Verify upgrades delete old files, including moving from "Program Files (x86)" to "Program Files".
+        using var upgradeTest = new UpgradeTest(platform);
+
         ExeInstaller.Install(platform, version, false, verbose);
         Verifier.RunVerificationTests(ProjectHelper.GetInstallationFolder(platform), verbose);
 
+        upgradeTest.Verify();
+
         var exePath = ProjectHelper.GetPackagePath("exe", platform, version);
         Console.WriteLine(verbose ? $"Verified exe installer: {exePath}" : "Done.");
+    }
+
+    private class UpgradeTest : IDisposable
+    {
+        private readonly Platform _platform;
+        private readonly string _install32;
+        private readonly string _testFilePath;
+
+        public UpgradeTest(Platform platform)
+        {
+            _platform = platform;
+            _install32 = ProjectHelper.GetInstallationFolder(Platform.Win32);
+            _testFilePath = Path.Combine(_install32, "_verify_testfile.exe");
+            Directory.CreateDirectory(_install32);
+            File.WriteAllText(_testFilePath, "");
+        }
+
+        public void Verify()
+        {
+            if (File.Exists(_testFilePath))
+            {
+                throw new Exception("Verification error: Exe installer did not delete old files");
+            }
+            if (_platform is Platform.Win or Platform.Win64 && Directory.Exists(_install32))
+            {
+                throw new Exception("Verification error: Exe installer did not delete old install dir");
+            }
+        }
+
+        public void Dispose()
+        {
+            try
+            {
+                File.Delete(_testFilePath);
+            }
+            catch (IOException)
+            {
+            }
+        }
     }
 }
