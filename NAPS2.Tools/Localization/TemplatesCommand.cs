7e47ad0dcedb92298627e6112f11886c252e6b7e
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2022-11-27 12:04:52 -0800

    Fix templates command

--- NAPS2.Tools/Localization/TemplatesCommand.cs
+++ NAPS2.Tools/Localization/TemplatesCommand.cs
@@ -7,9 +7,10 @@ public class TemplatesCommand : ICommand<TemplatesOptions>
     public int Run(TemplatesOptions opts)
     {
         var ctx = new TemplatesContext();
-        ctx.Load(Path.Combine(Paths.SolutionRoot, @"NAPS2.Core\Lang\Resources"), false);
-        ctx.Load(Path.Combine(Paths.SolutionRoot, @"NAPS2.Core\WinForms"), true);
-        ctx.Save(Path.Combine(Paths.SolutionRoot, @"NAPS2.Core\Lang\po\templates.pot"));
+        ctx.Load(Path.Combine(Paths.SolutionRoot, @"NAPS2.Sdk\Lang\Resources"), false);
+        ctx.Load(Path.Combine(Paths.SolutionRoot, @"NAPS2.Lib\Lang\Resources"), false);
+        ctx.Load(Path.Combine(Paths.SolutionRoot, @"NAPS2.Lib.WinForms\WinForms"), true);
+        ctx.Save(Path.Combine(Paths.SolutionRoot, @"NAPS2.Lib\Lang\po\templates.pot"));
         return 0;
     }
 }
