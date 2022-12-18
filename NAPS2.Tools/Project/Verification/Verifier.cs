6103e56c3f1b422d8c7baea3271f5577c54a70f5
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2022-12-18 15:07:02 -0800

    Fix verification

--- NAPS2.Tools/Project/Verification/Verifier.cs
+++ NAPS2.Tools/Project/Verification/Verifier.cs
@@ -5,7 +5,7 @@ public static class Verifier
     public static void RunVerificationTests(string testRoot)
     {
         Output.Info($"Running verification tests in: {testRoot}");
-        Cli.Run("dotnet", "test NAPS2.App.Tests -f net462", new()
+        Cli.Run("dotnet", "test NAPS2.App.Tests", new()
         {
             { "NAPS2_TEST_ROOT", testRoot },
             { "NAPS2_TEST_VERIFY", "1" }
