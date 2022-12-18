1bb11fb62103ec7d538317f9dd4793310dcf97f1
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2022-12-18 13:58:41 -0800

    WinForms: Fix app tests

--- NAPS2.App.Tests/Targets/WinNet462AppTestTarget.cs
+++ NAPS2.App.Tests/Targets/WinNet462AppTestTarget.cs
@@ -2,14 +2,14 @@ namespace NAPS2.App.Tests.Targets;
 
 public class WinNet462AppTestTarget : IAppTestTarget
 {
-    public AppTestExe Console => GetAppTestExe("NAPS2.Console.exe");
-    public AppTestExe Gui => GetAppTestExe("NAPS2.exe");
-    public AppTestExe Worker => GetAppTestExe("NAPS2.Worker.exe");
+    public AppTestExe Console => GetAppTestExe("NAPS2.App.Console", "NAPS2.Console.exe");
+    public AppTestExe Gui => GetAppTestExe("NAPS2.App.WinForms", "NAPS2.exe");
+    public AppTestExe Worker => GetAppTestExe("NAPS2.App.Worker", "NAPS2.Worker.exe");
 
-    private AppTestExe GetAppTestExe(string exeName)
+    private AppTestExe GetAppTestExe(string project, string exeName)
     {
         return new AppTestExe(
-            Path.Combine(AppTestHelper.SolutionRoot, "NAPS2.App.WinForms", "bin", "Debug", "net462"),
+            Path.Combine(AppTestHelper.SolutionRoot, project, "bin", "Debug", "net462"),
             exeName,
             TestRootSubPath: "lib");
     }
