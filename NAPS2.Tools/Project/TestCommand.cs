cffa067044a84645e7a9b6a4f17e1bdd8bfc56eb
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2022-12-24 21:04:53 -0800

    Fix test deps path on Mac/Linux

--- NAPS2.Tools/Project/TestCommand.cs
+++ NAPS2.Tools/Project/TestCommand.cs
@@ -1,3 +1,5 @@
+using System.Runtime.InteropServices;
+
 namespace NAPS2.Tools.Project;
 
 public class TestCommand : ICommand<TestOptions>
@@ -5,10 +7,11 @@ public class TestCommand : ICommand<TestOptions>
     public int Run(TestOptions opts)
     {
         // TODO: Framework options (e.g. "-f net462")
+        var arch = RuntimeInformation.OSArchitecture.ToString().ToLowerInvariant();
         var depsRootPath = OperatingSystem.IsMacOS()
-            ? "NAPS2.App.Mac/bin/Debug/net7-macos10.15"
+            ? $"NAPS2.App.Mac/bin/Debug/net7-macos10.15/osx-{arch}"
             : OperatingSystem.IsLinux()
-                ? "NAPS2.App.Gtk/bin/Debug/net6"
+                ? $"NAPS2.App.Gtk/bin/Debug/net6/linux-{arch}"
                 : "NAPS2.App.WinForms/bin/Debug/net462";
         Output.Info("Running tests");
         Cli.Run("dotnet", "test", new()
