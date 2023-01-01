988e62a5bca3c84277144ae9b86fffa7bf22ec2d
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2022-12-31 18:50:28 -0800

    Non-zero return code for clean command on failures

--- NAPS2.Tools/Project/CleanCommand.cs
+++ NAPS2.Tools/Project/CleanCommand.cs
@@ -5,6 +5,7 @@ public class CleanCommand : ICommand<CleanOptions>
     public int Run(CleanOptions opts)
     {
         Output.Info("Starting clean");
+        int rc = 0;
         foreach (var projectDir in new DirectoryInfo(Paths.SolutionRoot).EnumerateDirectories("NAPS2.*")
                      .Where(x => x.Name.ToLower() != "naps2.tools"))
         {
@@ -20,12 +21,13 @@ public class CleanCommand : ICommand<CleanOptions>
                     catch (Exception ex)
                     {
                         Output.Info($"Could not delete {projectDir.Name}/{cleanDir.Name}/{subDir.Name}: {ex.Message}");
+                        rc = 1;
                     }
                 }
             }
             Output.Verbose($"Cleaned {projectDir.Name}");
         }
-        Output.Info("Cleaned.");
-        return 0;
+        Output.Info(rc == 0 ? "Cleaned." : "Cleaned with failures.");
+        return rc;
     }
 }
