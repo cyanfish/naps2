d1840493d161e322a944f88c1dc0b6b2dc421206
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2023-01-01 19:05:52 -0800

    Fix worker initialization

--- NAPS2.Lib/Modules/WorkerModule.cs
+++ NAPS2.Lib/Modules/WorkerModule.cs
@@ -11,6 +11,15 @@ public class WorkerModule : Module
         builder.Register(ctx => new ScanningContext(ctx.Resolve<ImageContext>())).SingleInstance();
 #if MAC
         builder.RegisterType<StubTwainSessionController>().As<ITwainSessionController>();
+#elif NET6_0_OR_GREATER
+        if (OperatingSystem.IsWindows())
+        {
+            builder.RegisterType<LocalTwainSessionController>().As<ITwainSessionController>();
+        }
+        else
+        {
+            builder.RegisterType<StubTwainSessionController>().As<ITwainSessionController>();
+        }
 #else
         builder.RegisterType<LocalTwainSessionController>().As<ITwainSessionController>();
 #endif
