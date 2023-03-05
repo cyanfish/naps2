9458d60e0d613503d87c9263768a8b7e04adb702
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2023-03-05 09:47:11 -0800

    Use list patterns in TwainMemoryBufferReader

--- NAPS2.Sdk/Scan/Internal/Twain/TwainMemoryBufferReader.cs
+++ NAPS2.Sdk/Scan/Internal/Twain/TwainMemoryBufferReader.cs
@@ -13,32 +13,21 @@ public static class TwainMemoryBufferReader
     public static void CopyBufferToImage(TwainMemoryBuffer memoryBuffer, TwainImageData imageData,
         IMemoryImage outputImage)
     {
-        var subPixelType = ((PixelType) imageData.PixelType, imageData.BitsPerPixel, imageData.SamplesPerPixel) switch
-        {
-            (PixelType.RGB, 24, 3) when CheckBitsPerSample(imageData, 8, 8, 8) => SubPixelType.Rgb,
-            (PixelType.Gray, 8, 1) when CheckBitsPerSample(imageData, 8) => SubPixelType.Gray,
-            (PixelType.BlackWhite, 1, 1) when CheckBitsPerSample(imageData, 1) => SubPixelType.Bit,
-            _ => throw new ArgumentException(
-                $"Unsupported pixel type: {imageData.BitsPerPixel} {imageData.PixelType} {imageData.SamplesPerPixel} {string.Join(",", imageData.BitsPerSample)}")
-        };
+        var subPixelType = ((PixelType) imageData.PixelType, imageData.BitsPerPixel, imageData.SamplesPerPixel,
+                imageData.BitsPerSample) switch
+            {
+                (PixelType.RGB, 24, 3, [8, 8, 8, ..]) => SubPixelType.Rgb,
+                (PixelType.Gray, 8, 1, [8, ..]) => SubPixelType.Gray,
+                (PixelType.BlackWhite, 1, 1, [1, ..]) => SubPixelType.Bit,
+                _ => throw new ArgumentException(
+                    $"Unsupported pixel type: {imageData.BitsPerPixel} {imageData.PixelType} {imageData.SamplesPerPixel} {string.Join(",", imageData.BitsPerSample)}")
+            };
         var pixelInfo = new PixelInfo(memoryBuffer.Columns, memoryBuffer.Rows, subPixelType, memoryBuffer.BytesPerRow);
         new CopyBitwiseImageOp
         {
             DestXOffset = memoryBuffer.XOffset,
             DestYOffset = memoryBuffer.YOffset
         }.Perform(memoryBuffer.Buffer.ToByteArray(), pixelInfo, outputImage);
-    }
-
-    private static bool CheckBitsPerSample(TwainImageData imageData, params int[] expected)
-    {
-        for (int i = 0; i < expected.Length; i++)
-        {
-            if (imageData.BitsPerSample[i] != expected[i])
-            {
-                return false;
-            }
-        }
-        return true;
     }
 }
 #endif
