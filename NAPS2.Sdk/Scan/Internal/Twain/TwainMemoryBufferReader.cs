586932d121264d98265081cd31a4d6b781d9c39a
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2023-03-09 12:52:48 -0800

    Twain: Loosen bits-per-sample check

--- NAPS2.Sdk/Scan/Internal/Twain/TwainMemoryBufferReader.cs
+++ NAPS2.Sdk/Scan/Internal/Twain/TwainMemoryBufferReader.cs
@@ -16,7 +16,7 @@ public static class TwainMemoryBufferReader
         var subPixelType = ((PixelType) imageData.PixelType, imageData.BitsPerPixel, imageData.SamplesPerPixel,
                 imageData.BitsPerSample) switch
             {
-                (PixelType.RGB, 24, 3, [8, 8, 8, ..]) => SubPixelType.Rgb,
+                (PixelType.RGB, 24, 3, [8, ..]) => SubPixelType.Rgb,
                 (PixelType.Gray, 8, 1, [8, ..]) => SubPixelType.Gray,
                 (PixelType.BlackWhite, 1, 1, [1, ..]) => SubPixelType.Bit,
                 _ => throw new ArgumentException(
