2a1e922b79afac051e7c24bd9539c2775a763ec8
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2023-03-19 20:48:33 +0000

    Use libdl for library loading on Linux
    Fixes #112

--- NAPS2.Sdk/Platform/Linux/LinuxInterop.cs
+++ NAPS2.Sdk/Platform/Linux/LinuxInterop.cs
@@ -4,13 +4,13 @@ namespace NAPS2.Platform.Linux;
 
 public static class LinuxInterop
 {
-    [DllImport("libc.so.6")]
+    [DllImport("libdl.so.2")]
     public static extern IntPtr dlopen(string filename, int flags);
 
-    [DllImport("libc.so.6")]
+    [DllImport("libdl.so.2")]
     public static extern string dlerror();
 
-    [DllImport("libc.so.6")]
+    [DllImport("libdl.so.2")]
     public static extern IntPtr dlsym(IntPtr handle, string symbol);
 
     [DllImport("libc.so.6")]
