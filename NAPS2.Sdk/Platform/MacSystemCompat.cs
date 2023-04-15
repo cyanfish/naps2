07550af2eeedb39f595dd7439198ecdcd893ca65
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2023-04-15 12:51:53 -0700

    Remember background operations on Mac and Linux

--- NAPS2.Sdk/Platform/MacSystemCompat.cs
+++ NAPS2.Sdk/Platform/MacSystemCompat.cs
@@ -22,7 +22,7 @@ public class MacSystemCompat : ISystemCompat
 
     public bool CanPrint => true;
 
-    public bool ShouldRememberBackgroundOperations => false;
+    public bool ShouldRememberBackgroundOperations => true;
 
     public bool UseSystemTesseract => false;
 
