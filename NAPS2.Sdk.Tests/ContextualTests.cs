8b50ad3baa737e232dd0e03ab4322ea7d4121a76
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2023-04-22 10:55:55 -0700

    Write logged exceptions to test output

--- NAPS2.Sdk.Tests/ContextualTests.cs
+++ NAPS2.Sdk.Tests/ContextualTests.cs
@@ -168,6 +168,10 @@ public class ContextualTests : IDisposable
         public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
         {
             _testOutputHelper.WriteLine(state.ToString());
+            if (exception != null)
+            {
+                _testOutputHelper.WriteLine(exception.ToString());
+            }
         }
 
         public bool IsEnabled(LogLevel logLevel)
