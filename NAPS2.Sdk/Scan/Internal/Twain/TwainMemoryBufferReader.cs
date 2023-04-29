ada7f5bdf33a82f34837f570107c9b5ae6bc2d26
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2023-04-29 16:35:16 -0700

    Avoid direct NTwain dependency in TwainMemoryBufferReader

--- NAPS2.Sdk/Scan/Internal/Twain/TwainMemoryBufferReader.cs
+++ NAPS2.Sdk/Scan/Internal/Twain/TwainMemoryBufferReader.cs
@@ -1,7 +1,6 @@
 #if !MAC
 using NAPS2.Images.Bitwise;
 using NAPS2.Remoting.Worker;
-using NTwain.Data;
 
 namespace NAPS2.Scan.Internal.Twain;
 
@@ -10,16 +9,20 @@ namespace NAPS2.Scan.Internal.Twain;
 /// </summary>
 public static class TwainMemoryBufferReader
 {
+    private const int BLACK_WHITE = 0;
+    private const int GRAY = 1;
+    private const int RGB = 2;
+
     public static void CopyBufferToImage(TwainMemoryBuffer memoryBuffer, TwainImageData imageData,
         IMemoryImage outputImage)
     {
-        var subPixelType = ((PixelType) imageData.PixelType, imageData.BitsPerPixel, imageData.SamplesPerPixel,
+        var subPixelType = (imageData.PixelType, imageData.BitsPerPixel, imageData.SamplesPerPixel,
                 imageData.BitsPerSample) switch
             {
                 // Technically for RGB we should check for [8, 8, 8, ...] but some scanners only set the first value
-                (PixelType.RGB, 24, 3, [8, ..]) => SubPixelType.Rgb,
-                (PixelType.Gray, 8, 1, [8, ..]) => SubPixelType.Gray,
-                (PixelType.BlackWhite, 1, 1, [1, ..]) => SubPixelType.Bit,
+                (RGB, 24, 3, [8, ..]) => SubPixelType.Rgb,
+                (GRAY, 8, 1, [8, ..]) => SubPixelType.Gray,
+                (BLACK_WHITE, 1, 1, [1, ..]) => SubPixelType.Bit,
                 _ => throw new ArgumentException(
                     $"Unsupported pixel type: {imageData.BitsPerPixel} {imageData.PixelType} {imageData.SamplesPerPixel} {string.Join(",", imageData.BitsPerSample)}")
             };
