c2816e53d3b044b035bd53ad5c8ac4754a3eaf2d
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2023-09-04 13:50:05 -0700

    Escl: Combine Escl.Client project with Escl project

--- NAPS2.Escl.Tests/DeviceServiceLocatorTests.cs
+++ NAPS2.Escl.Tests/DeviceServiceLocatorTests.cs
@@ -1,13 +1,13 @@
-using NAPS2.Escl.Client;
-using Xunit;
-
-namespace NAPS2.Escl.Tests;
-
-public class DeviceServiceLocatorTests
-{
-    [Fact]
-    public async Task Locate()
-    {
-        // await new EsclServiceLocator().Locate();
-    }
-}
+// using NAPS2.Escl.Client;
+// using Xunit;
+//
+// namespace NAPS2.Escl.Tests;
+//
+// public class DeviceServiceLocatorTests
+// {
+//     [Fact]
+//     public async Task Locate()
+//     {
+//         await new EsclServiceLocator().Locate();
+//     }
+// }
