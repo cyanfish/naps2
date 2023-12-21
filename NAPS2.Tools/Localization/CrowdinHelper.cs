b1df448946599e18000fec5bf273746a18f67d3f
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2023-12-21 10:49:53 -0800

    Tools: Trim crowdin api key

--- NAPS2.Tools/Localization/CrowdinHelper.cs
+++ NAPS2.Tools/Localization/CrowdinHelper.cs
@@ -9,7 +9,7 @@ public static class CrowdinHelper
 
     public static CrowdinApiClient GetClient()
     {
-        var key = File.ReadAllText(Path.Combine(Paths.Naps2UserFolder, "crowdin"));
+        var key = File.ReadAllText(Path.Combine(Paths.Naps2UserFolder, "crowdin")).Trim();
         var client = new CrowdinApiClient(new CrowdinCredentials { AccessToken = key });
         return client;
     }
