c7bd48442a5286e3a777b0a6e89cae49312e4253
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2023-12-21 10:46:42 -0800

    Tools: Fix path separators for templates command

--- NAPS2.Tools/Localization/TemplatesCommand.cs
+++ NAPS2.Tools/Localization/TemplatesCommand.cs
@@ -1,14 +1,12 @@
-﻿using NAPS2.Tools.Project;
-
-namespace NAPS2.Tools.Localization;
+﻿namespace NAPS2.Tools.Localization;
 
 public class TemplatesCommand : ICommand<TemplatesOptions>
 {
     public int Run(TemplatesOptions opts)
     {
         var ctx = new TemplatesContext();
-        ctx.Load(Path.Combine(Paths.SolutionRoot, @"NAPS2.Sdk\Lang\Resources"), false);
-        ctx.Load(Path.Combine(Paths.SolutionRoot, @"NAPS2.Lib\Lang\Resources"), false);
+        ctx.Load(Path.Combine(Paths.SolutionRoot, "NAPS2.Sdk", "Lang", "Resources"), false);
+        ctx.Load(Path.Combine(Paths.SolutionRoot, "NAPS2.Lib", "Lang", "Resources"), false);
         ctx.Save(Paths.TemplatesFile);
         return 0;
     }
