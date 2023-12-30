a13aca995cf4fa8e54df7c11978475c0cec0f362
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2023-12-30 11:59:34 -0800

    Add xml docs

--- NAPS2.Sdk/Scan/BarcodeDetectionOptions.cs
+++ NAPS2.Sdk/Scan/BarcodeDetectionOptions.cs
@@ -2,6 +2,9 @@ using ZXing.Common;
 
 namespace NAPS2.Scan;
 
+/// <summary>
+/// Options for detecting barcodes using ZXing.
+/// </summary>
 public class BarcodeDetectionOptions
 {
     public bool DetectBarcodes { get; set; }
--- NAPS2.Sdk/Scan/ScanningContextExtensions.cs
+++ NAPS2.Sdk/Scan/ScanningContextExtensions.cs
@@ -4,6 +4,10 @@ namespace NAPS2.Scan;
 
 public static class ScanningContextExtensions
 {
+    /// <summary>
+    /// If you have installed the NAPS2.Sdk.Worker.Win32 Nuget package, call this method to set up the worker on the
+    /// ScanningContext. This will allow 32-bit TWAIN drivers to be used.
+    /// </summary>
     public static void SetUpWin32Worker(this ScanningContext scanningContext)
     {
         var workerFactory = WorkerFactory.CreateDefault();
