dd43092fe22a9b9844077020f01cf0141730f410
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2023-12-29 19:49:06 -0800

    Docfx WIP

--- NAPS2.Tools/Project/DocCommand.cs
+++ NAPS2.Tools/Project/DocCommand.cs
@@ -4,6 +4,14 @@ public class DocCommand : ICommand<DocOptions>
 {
     public int Run(DocOptions options)
     {
+        File.Copy(
+            Path.Combine(Paths.SolutionRoot, "NAPS2.Sdk", "README.md"),
+            Path.Combine(Paths.SolutionRoot, "NAPS2.Sdk", "_doc", "index.md"),
+            true);
+        if (options.DocCommand == "build")
+        {
+            Cli.Run("docfx", "NAPS2.Sdk/_doc/docfx.json", alwaysVerbose: true);
+        }
         if (options.DocCommand == "serve")
         {
             Cli.Run("docfx", "NAPS2.Sdk/_doc/docfx.json --serve", alwaysVerbose: true);
--- NAPS2.Tools/Project/DocOptions.cs
+++ NAPS2.Tools/Project/DocOptions.cs
@@ -5,6 +5,6 @@ namespace NAPS2.Tools.Project;
 [Verb("doc", HelpText = "Docfx control")]
 public class DocOptions : OptionsBase
 {
-    [Value(0, MetaName = "doc command", Required = true, HelpText = "serve|?")]
+    [Value(0, MetaName = "doc command", Required = true, HelpText = "build|serve")]
     public string? DocCommand { get; set; }
 }
