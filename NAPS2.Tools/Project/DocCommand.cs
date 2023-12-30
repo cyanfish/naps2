6a3258c9421f6a1b2132a88852dfc04d97e11a83
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2023-12-29 21:11:02 -0800

    Docfx fixes

--- NAPS2.Tools/Project/DocCommand.cs
+++ NAPS2.Tools/Project/DocCommand.cs
@@ -4,10 +4,20 @@ public class DocCommand : ICommand<DocOptions>
 {
     public int Run(DocOptions options)
     {
+        // Clean up old build
+        Directory.Delete(Path.Combine(Paths.SolutionRoot, "NAPS2.Sdk", "_doc", "_site"), true);
+        foreach (var file in new DirectoryInfo(Path.Combine(Paths.SolutionRoot, "NAPS2.Sdk", "_doc", "api"))
+                     .EnumerateFiles().Where(x => x.Name != ".gitignore"))
+        {
+            file.Delete();
+        }
+
+        // Copy the SDK readme as index.html
         File.Copy(
             Path.Combine(Paths.SolutionRoot, "NAPS2.Sdk", "README.md"),
-            Path.Combine(Paths.SolutionRoot, "NAPS2.Sdk", "_doc", "index.md"),
+            Path.Combine(Paths.SolutionRoot, "NAPS2.Sdk", "_doc", "api", "index.md"),
             true);
+
         if (options.DocCommand == "build")
         {
             Cli.Run("docfx", "NAPS2.Sdk/_doc/docfx.json", alwaysVerbose: true);
