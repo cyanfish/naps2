1a921a0244c76a6a421764916f597d523d710b30
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2024-01-20 21:45:19 -0800

    Comprehensive language->script->font mappings

--- NAPS2.Sdk.Tests/Pdf/PdfFontTests.cs
+++ NAPS2.Sdk.Tests/Pdf/PdfFontTests.cs
@@ -5,6 +5,7 @@ using NAPS2.Sdk.Tests.Asserts;
 using PdfSharpCore.Utils;
 using Xunit;
 using Xunit.Abstractions;
+using Alphabet = NAPS2.Pdf.PdfFontPicker.Alphabet;
 
 namespace NAPS2.Sdk.Tests.Pdf;
 
@@ -35,7 +36,14 @@ public class PdfFontTests : ContextualTests
 
     [Theory]
     [MemberData(nameof(AlphabetTestCases))]
-    public async Task ExportAlphabetsWithPdfSharp(string langCode, string text, bool rtl)
+    internal async Task MapLanguageCodeToAlphabet(Alphabet alphabet, string langCode, string text, bool rtl)
+    {
+        Assert.Equal(alphabet, PdfFontPicker.MapLanguageCodeToAlphabet(langCode));
+    }
+
+    [Theory]
+    [MemberData(nameof(AlphabetTestCases))]
+    internal async Task ExportAlphabetsWithPdfSharp(Alphabet alphabet, string langCode, string text, bool rtl)
     {
         SetUpFakeOcr(new()
         {
@@ -54,7 +62,7 @@ public class PdfFontTests : ContextualTests
 
     [Theory]
     [MemberData(nameof(AlphabetTestCases))]
-    public async Task ExportAlphabetsWithPdfium(string langCode, string text, bool rtl)
+    internal async Task ExportAlphabetsWithPdfium(Alphabet alphabet, string langCode, string text, bool rtl)
     {
         SetUpFakeOcr(new()
         {
@@ -73,14 +81,34 @@ public class PdfFontTests : ContextualTests
 
     public static IEnumerable<object[]> AlphabetTestCases =
     [
-        new object[] { "eng", "Hello world", false },
-        new object[] { "heb", "שלום עולם", true },
-        new object[] { "ell", "Γειά σου Κόσμε", false },
-        new object[] { "rus", "Привет, мир", false },
-        new object[] { "kor", "안녕하세요 세상", false },
-        new object[] { "chi_sim", "你好复杂的世界", false },
-        new object[] { "chi_tra", "你好複雜的世界", false },
-        new object[] { "jpn", "こんにちは世界", false },
-        new object[] { "ara", "مرحبا بالعالم", true }
+        new object[] { Alphabet.Latin, "eng", "Hello world", false },
+        new object[] { Alphabet.Cyrillic, "rus", "Привет, мир", false },
+        new object[] { Alphabet.Greek, "ell", "Γειά σου Κόσμε", false },
+        new object[] { Alphabet.Hebrew, "heb", "שלום עולם", true },
+        new object[] { Alphabet.Arabic, "ara", "مرحبا بالعالم", true },
+        new object[] { Alphabet.Armenian, "hye", "Բարեւ աշխարհ", false },
+        new object[] { Alphabet.Bengali, "ben", "ওহে বিশ্ব", false },
+        new object[] { Alphabet.CanadianAboriginal, "iku", "ᐃᓄᒃᑎᑐᑦ", false },
+        new object[] { Alphabet.Cherokee, "chr", "ᏣᎳᎩ ᎦᏬᏂᎯᏍᏗ", false },
+        new object[] { Alphabet.Devanagari, "hin", "ह\u0948ल\u094b वर\u094dल\u094dड", false },
+        new object[] { Alphabet.Ethiopic, "amh", "ሰላም ልዑል", false },
+        new object[] { Alphabet.Georgian, "kat", "Გამარჯობა მსოფლიო", false },
+        new object[] { Alphabet.Gujarati, "guj", "હ\u0ac7લ\u0acb વર\u0acdલ\u0acdડ", false },
+        new object[] { Alphabet.Gurmukhi, "pan", "ਸਤ\u0a3f ਸ\u0a4dਰ\u0a40 ਅਕ\u0a3eਲ ਦ\u0a41ਨ\u0a3fਆ", false },
+        new object[] { Alphabet.Kannada, "kan", "ಹಲ\u0ccbವರ\u0ccdಲ\u0ccdಡ\u0ccd", false },
+        new object[] { Alphabet.Khmer, "khm", "ស\u17bdស\u17d2ត\u17b8\u200bព\u17b7ភពល\u17c4ក", false },
+        new object[] { Alphabet.Lao, "lao", "ສະ\u200bບາຍ\u200bດ\u0eb5\u200bຊາວ\u200bໂລກ", false },
+        new object[] { Alphabet.Malayalam, "mal", "ഹല\u0d47\u0d3e വ\u0d47ൾഡ\u0d4d", false },
+        new object[] { Alphabet.Myanmar, "mya", "မင\u103a\u1039ဂလ\u102cပ\u102bကမ\u1039ဘ\u102cလ\u1031\u102cက", false },
+        new object[] { Alphabet.Sinhala, "sin", "හ\u0dd9ල\u0dddවර\u0dcaල\u0dcaඩ\u0dca", false },
+        new object[] { Alphabet.Tamil, "tam", "வணக\u0bcdகம\u0bcdஉலகம\u0bcd", false },
+        new object[] { Alphabet.Telugu, "tel", "హల\u0c4bవరల\u0c4dడ\u0c4d", false },
+        new object[] { Alphabet.Thaana, "div", "ހ\u07acލ\u07afދ\u07aaނ\u07a8ޔ\u07ac", true },
+        new object[] { Alphabet.Thai, "tha", "สว\u0e31สด\u0e35ชาวโลก", false },
+        new object[] { Alphabet.Tibetan, "bod", "བ\u0f7cད་ས\u0f90ད་", false },
+        new object[] { Alphabet.ChineseSimplified, "chi_sim", "你好复杂的世界", false },
+        new object[] { Alphabet.ChineseTraditional, "chi_tra", "你好複雜的世界", false },
+        new object[] { Alphabet.Japanese, "jpn", "こんにちは世界", false },
+        new object[] { Alphabet.Korean, "kor", "안녕하세요 세상", false },
     ];
 }
--- NAPS2.Sdk/Pdf/PdfFontPicker.cs
+++ NAPS2.Sdk/Pdf/PdfFontPicker.cs
@@ -12,15 +12,7 @@ internal static class PdfFontPicker
         // This logic is incomplete, but the goal is to get PdfFontTests passing, which works as
         // the default font supports multiple scripts. e.g. Times New Roman supports most everything
         // except CJK (Chinese-Japanese-Korean)
-        var alphabet = languageCode.ToLowerInvariant() switch
-        {
-            "chi_sim" or "chi_sim_vert" => Alphabet.ChineseSimplified,
-            "chi_tra" or "chi_tra_vert" => Alphabet.ChineseTraditional,
-            "jpn" or "jpn_vert" => Alphabet.Japanese,
-            "kor" or "kor_vert" => Alphabet.Korean,
-            "ara" or "fas" or "msa" or "snd" or "urd" => Alphabet.Arabic,
-            _ => Alphabet.Unknown
-        };
+        var alphabet = MapLanguageCodeToAlphabet(languageCode);
 
 #if NET6_0_OR_GREATER
         if (OperatingSystem.IsMacOS())
@@ -39,6 +31,29 @@ internal static class PdfFontPicker
     {
         return alphabet switch
         {
+            // See https://learn.microsoft.com/en-us/typography/fonts/windows_10_font_list
+            Alphabet.Bengali => "Nirmala UI",
+            Alphabet.CanadianAboriginal => "Gadugi",
+            Alphabet.Cherokee => "Gadugi",
+            Alphabet.Devanagari => "Nirmala UI",
+            Alphabet.Ethiopic => "Ebrima",
+            // Alphabet.Fraktur => "",
+            Alphabet.Georgian => "Calibri",
+            Alphabet.Gujarati => "Nirmala UI",
+            Alphabet.Gurmukhi => "Nirmala UI",
+            Alphabet.Kannada => "Nirmala UI",
+            Alphabet.Khmer => "Leelawadee UI",
+            Alphabet.Lao => "Leelawadee UI",
+            Alphabet.Malayalam => "Nirmala UI",
+            Alphabet.Myanmar => "Myanmar Text",
+            // Alphabet.Oriya => "",
+            Alphabet.Sinhala => "Nirmala UI",
+            // Alphabet.Syriac => "",
+            Alphabet.Tamil => "Nirmala UI",
+            Alphabet.Telugu => "Nirmala UI",
+            Alphabet.Thaana => "MV Boli",
+            Alphabet.Thai => "Leelawadee UI",
+            Alphabet.Tibetan => "Microsoft Himalaya",
             Alphabet.ChineseSimplified => "Microsoft YaHei",
             Alphabet.ChineseTraditional => "Microsoft JhengHei",
             Alphabet.Japanese => "MS Gothic",
@@ -57,11 +72,33 @@ internal static class PdfFontPicker
         return alphabet switch
         {
             // Noto fonts aren't always going to be installed, but they're among the most common
+            Alphabet.Arabic => "Noto Sans Arabic",
+            Alphabet.Bengali => "Noto Sans Bengali",
+            Alphabet.CanadianAboriginal => "Noto Sans CanAborig",
+            Alphabet.Cherokee => "Noto Sans Cherokee",
+            Alphabet.Devanagari => "Noto Sans Devanagari",
+            Alphabet.Ethiopic => "Noto Sans Ethiopic",
+            // Alphabet.Fraktur => "",
+            Alphabet.Georgian => "Noto Sans Georgian",
+            Alphabet.Gujarati => "Noto Sans Gujarati",
+            Alphabet.Gurmukhi => "Noto Sans Gurmukhi",
+            Alphabet.Kannada => "Noto Sans Kannada",
+            Alphabet.Khmer => "Noto Sans Khmer",
+            Alphabet.Lao => "Noto Sans Lao",
+            Alphabet.Malayalam => "Noto Sans Malayalam",
+            Alphabet.Myanmar => "Noto Sans Myanmar",
+            Alphabet.Oriya => "Noto Sans Oriya",
+            Alphabet.Sinhala => "Noto Sans Sinhala",
+            Alphabet.Syriac => "Noto Sans Syriac",
+            Alphabet.Tamil => "Noto Sans Tamil",
+            Alphabet.Telugu => "Noto Sans Telugu",
+            Alphabet.Thaana => "Noto Sans Thaana",
+            Alphabet.Thai => "Noto Sans Thai",
+            Alphabet.Tibetan => "Noto Serif Tibetan",
             Alphabet.ChineseSimplified => "Noto Sans CJK SC",
             Alphabet.ChineseTraditional => "Noto Sans CJK TC",
             Alphabet.Japanese => "Noto Sans CJK JP",
             Alphabet.Korean => "Noto Sans CJK KR",
-            Alphabet.Arabic => "Noto Sans Arabic",
             // Liberation Serif is broadly included in Linux distros and is designed to have the same measurements
             // as Times New Roman.
             // TODO: Maybe we should use Times New Roman if available?
@@ -72,14 +109,87 @@ internal static class PdfFontPicker
     public enum Alphabet
     {
         Unknown,
+        // Common (supported by Times New Roman / Liberation Serif)
         Latin,
-        Arabic,
-        Hebrew,
         Cyrillic,
         Greek,
+        Hebrew,
+        // Uncommon (needs special handling)
+        Arabic,
+        Armenian,
+        Bengali,
+        CanadianAboriginal,
+        Cherokee,
+        Devanagari,
+        Ethiopic,
+        Fraktur,
+        Georgian,
+        Gujarati,
+        Gurmukhi,
+        Kannada,
+        Khmer,
+        Lao,
+        Malayalam,
+        Myanmar,
+        Oriya,
+        Sinhala,
+        Syriac,
+        Tamil,
+        Telugu,
+        Thaana,
+        Thai,
+        Tibetan,
+        // CJK (needs special handling)
         ChineseSimplified,
         ChineseTraditional,
         Japanese,
         Korean
+    }
+
+    // Reference: https://github.com/tesseract-ocr/langdata_lstm/tree/main/script
+    public static Alphabet MapLanguageCodeToAlphabet(string languageCode)
+    {
+        return languageCode.ToLowerInvariant() switch
+        {
+            "ara" or "fas" or "kur_ara" or "pus" or "snd" or "uig" or "urd" => Alphabet.Arabic,
+            "hye" => Alphabet.Armenian,
+            "asm" or "ben" => Alphabet.Bengali,
+            "iku" => Alphabet.CanadianAboriginal,
+            "chr" => Alphabet.Cherokee,
+            "aze_cyrl" or "bel" or "bul" or "kaz" or "kir" or "mkd" or "mon" or "rus" or "srp" or "tgk" or "ukr" or "uzb_cyrl" => Alphabet.Cyrillic,
+            "hin" or "mar" or "nep" or "san" => Alphabet.Devanagari,
+            "amh" or "tir" => Alphabet.Ethiopic,
+            "enm" or "frm" or "frk" or "ita_old" or "spa_old" => Alphabet.Fraktur,
+            "kat" or "kat_old" => Alphabet.Georgian,
+            "ell" or "grc" => Alphabet.Greek,
+            "guj" => Alphabet.Gujarati,
+            "pan" => Alphabet.Gurmukhi,
+            "heb" or "yid" => Alphabet.Hebrew,
+            "kan" => Alphabet.Kannada,
+            "khm" => Alphabet.Khmer,
+            "lao" => Alphabet.Lao,
+            // TODO: The reference says "vie" (Vietnamese) is its own script, but it looks like Latin?
+            "afr" or "aze" or "bos" or "bre" or "cat" or "ceb" or "ces" or "cos" or "cym" or "dan" or "deu" or "eng" or
+                "epo" or "est" or "eus" or "fao" or "fil" or "fin" or "fra" or "fry" or "gla" or "gle" or "glg" or
+                "hat" or "hrv" or "hun" or "ind" or "isl" or "ita" or "jav" or "lat" or "lav" or "lit" or "ltz" or
+                "mlt" or "mri" or "msa" or "nld" or "nor" or "oci" or "pol" or "por" or "que" or "ron" or "slk" or
+                "slv" or "spa" or "sqi" or "srp_latn" or "sun" or "swa" or "swe" or "tat" or "ton" or "tur" or "uzb" or
+                "vie" or "yor" => Alphabet.Latin,
+            "mal" => Alphabet.Malayalam,
+            "mya" => Alphabet.Myanmar,
+            "ori" => Alphabet.Oriya,
+            "sin" => Alphabet.Sinhala,
+            "syr" => Alphabet.Syriac,
+            "tam" => Alphabet.Tamil,
+            "tel" => Alphabet.Telugu,
+            "div" => Alphabet.Thaana,
+            "tha" => Alphabet.Thai,
+            "bod" or "dzo" => Alphabet.Tibetan,
+            "chi_sim" or "chi_sim_vert" => Alphabet.ChineseSimplified,
+            "chi_tra" or "chi_tra_vert" => Alphabet.ChineseTraditional,
+            "jpn" or "jpn_vert" => Alphabet.Japanese,
+            "kor" or "kor_vert" => Alphabet.Korean,
+            _ => Alphabet.Unknown
+        };
     }
 }
