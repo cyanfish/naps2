a9b0d9dbddd48fb3a3ba1fe1d437155b04bbe8f0
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2024-04-07 11:46:33 -0700

    Fix compile

--- NAPS2.Lib.Gtk/EtoForms/Ui/GtkPreviewForm.cs
+++ NAPS2.Lib.Gtk/EtoForms/Ui/GtkPreviewForm.cs
@@ -6,8 +6,8 @@ namespace NAPS2.EtoForms.Ui;
 public class GtkPreviewForm : PreviewForm
 {
     public GtkPreviewForm(Naps2Config config, DesktopCommands desktopCommands, UiImageList imageList,
-        IIconProvider iconProvider, KeyboardShortcutManager ksm, ColorScheme colorScheme) : base(config,
-        desktopCommands, imageList, iconProvider, ksm, colorScheme)
+        IIconProvider iconProvider, ColorScheme colorScheme) : base(config,
+        desktopCommands, imageList, iconProvider, colorScheme)
     {
     }
 
