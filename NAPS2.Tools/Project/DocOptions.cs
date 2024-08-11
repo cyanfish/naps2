430f12c4238f6455068cfe59f4c378a17a7821ae
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2024-08-10 18:54:49 -0700

    Tools: Fix doc help text

--- NAPS2.Tools/Project/DocOptions.cs
+++ NAPS2.Tools/Project/DocOptions.cs
@@ -5,6 +5,6 @@ namespace NAPS2.Tools.Project;
 [Verb("doc", HelpText = "Docfx control")]
 public class DocOptions : OptionsBase
 {
-    [Value(0, MetaName = "doc command", Required = true, HelpText = "build|serve")]
+    [Value(0, MetaName = "doc command", Required = true, HelpText = "build|serve|push")]
     public string? DocCommand { get; set; }
 }
