d1b3c8489800be8a04a9ec58c86187a8d1de159f
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2024-08-20 19:59:36 -0700

    Fix search path for worker in app tests

--- NAPS2.App.Tests/Targets/WindowsAppTestTarget.cs
+++ NAPS2.App.Tests/Targets/WindowsAppTestTarget.cs
@@ -2,15 +2,15 @@ namespace NAPS2.App.Tests.Targets;
 
 public class WindowsAppTestTarget : IAppTestTarget
 {
-    public AppTestExe Console => GetAppTestExe("NAPS2.App.Console", "NAPS2.Console.exe", null);
-    public AppTestExe Gui => GetAppTestExe("NAPS2.App.WinForms", "NAPS2.exe", null);
-    public AppTestExe Worker => GetAppTestExe("NAPS2.App.Worker", "NAPS2.Worker.exe", "lib");
+    public AppTestExe Console => GetAppTestExe("NAPS2.App.Console", "NAPS2.Console.exe", "win-x64", null);
+    public AppTestExe Gui => GetAppTestExe("NAPS2.App.WinForms", "NAPS2.exe", "win-x64", null);
+    public AppTestExe Worker => GetAppTestExe("NAPS2.App.Worker", "NAPS2.Worker.exe", "win-x86", "lib");
     public bool IsWindows => true;
 
-    private AppTestExe GetAppTestExe(string project, string exeName, string testRootSubPath)
+    private AppTestExe GetAppTestExe(string project, string exeName, string arch, string testRootSubPath)
     {
         return new AppTestExe(
-            Path.Combine(AppTestHelper.SolutionRoot, project, "bin", "Debug", "net9-windows", "win-x64"),
+            Path.Combine(AppTestHelper.SolutionRoot, project, "bin", "Debug", "net9-windows", arch),
             exeName,
             TestRootSubPath: testRootSubPath);
     }
