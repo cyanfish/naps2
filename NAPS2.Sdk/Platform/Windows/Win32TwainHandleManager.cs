3af9fce59c25fc0243128f8cd0e6f6d6df75799d
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2025-03-19 20:37:05 -0700

    Twain: Fix scanning with unspecified DialogParent

--- NAPS2.Sdk/Platform/Windows/Win32TwainHandleManager.cs
+++ NAPS2.Sdk/Platform/Windows/Win32TwainHandleManager.cs
@@ -36,28 +36,25 @@ internal class Win32TwainHandleManager : TwainHandleManager
 
     private IntPtr GetHandle(IntPtr dialogParent, bool useNativeUi)
     {
-        if (dialogParent == IntPtr.Zero)
-        {
-            // If we have no real parent, we just give it an arbitrary form handle in this process as a parent.
-            return _messagePump.Handle;
-        }
-
         // If we are expected to show UI, ideally we'd just return dialogParent. But I've found some issues with that
         // where the window can become non-interactable (e.g. unable to cancel a native UI scan). The cause might be
         // related to the window being in another process.
         _parentWindow = _messagePump.CreateBackgroundWindow(dialogParent);
 
-        // At the Windows API level, a modal window is implemented by doing two things:
-        // 1. Setting the parent on the child window
-        // 2. Disabling the parent window
-        // We do this rather than calling ShowDialog to avoid blocking the thread.
-        if (useNativeUi)
+        if (dialogParent != IntPtr.Zero)
         {
-            // We only want to disable the parent window if we're showing the native UI. Otherwise, we expect that
-            // the NAPS2 UI should be interactable, and the only UI shown should be error messages.
-            Win32.EnableWindow(dialogParent, false);
+            // At the Windows API level, a modal window is implemented by doing two things:
+            // 1. Setting the parent on the child window
+            // 2. Disabling the parent window
+            // We do this rather than calling ShowDialog to avoid blocking the thread.
+            if (useNativeUi)
+            {
+                // We only want to disable the parent window if we're showing the native UI. Otherwise, we expect that
+                // the NAPS2 UI should be interactable, and the only UI shown should be error messages.
+                Win32.EnableWindow(dialogParent, false);
+            }
+            _disabledWindow = dialogParent;
         }
-        _disabledWindow = dialogParent;
 
         return _parentWindow;
     }
