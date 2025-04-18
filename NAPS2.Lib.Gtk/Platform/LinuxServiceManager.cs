683b5984118b30c21919e3f13f0eee66fc22d9ce
Ben Olden-Cooligan <ben.cyanfish@gmail.com>
2025-04-18 06:42:54 -0700

    Linux background sharing via systemd

--- NAPS2.Lib.Gtk/Platform/LinuxServiceManager.cs
+++ NAPS2.Lib.Gtk/Platform/LinuxServiceManager.cs
@@ -5,16 +5,72 @@ namespace NAPS2.Platform;
 /// </summary>
 public class LinuxServiceManager : IOsServiceManager
 {
-    // At the moment we only support systemd on Linux
-    public bool CanRegister => Directory.Exists(" /run/systemd/system/");
+    // At the moment we only support systemd on Linux and without Flatpak
+    public bool CanRegister => Directory.Exists("/run/systemd/system/") && !File.Exists("/.flatpak-info");
 
-    public bool IsRegistered => false;
+    private static string UnitPath =>
+        Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
+            ".config/systemd/user/naps2-sharing-server.service");
+    
+    public bool IsRegistered => File.Exists(UnitPath);
 
     public void Register()
     {
+        var unitDef = $"""
+                          [Unit]
+                          Description=NAPS2 Scanner Sharing Server
+                          
+                          [Service]
+                          Type=simple
+                          Restart=always
+                          RestartSec=1
+                          ExecStart={Environment.ProcessPath} server
+                          """;
+        try
+        {
+            Directory.CreateDirectory(Path.GetDirectoryName(UnitPath)!);
+            File.WriteAllText(UnitPath, unitDef);
+        }
+        catch (Exception ex)
+        {
+            Log.ErrorException($"Error creating systemd unit: {UnitPath}", ex);
+        }
+        if (!ProcessHelper.TryRun("systemctl", "--user daemon-reload", 1000))
+        {
+            Log.Error("Could not run systemctl daemon-reload");
+        }
+        if (!ProcessHelper.TryRun("systemctl", "--user enable naps2-sharing-server", 1000))
+        {
+            Log.Error("Could not enable service naps2-sharing-server");
+        }
+        if (!ProcessHelper.TryRun("systemctl", "--user start naps2-sharing-server", 1000))
+        {
+            Log.Error("Could not start service naps2-sharing-server");
+        }
     }
 
     public void Unregister()
     {
+        if (!ProcessHelper.TryRun("systemctl", "--user stop naps2-sharing-server", 1000))
+        {
+            Log.Error("Could not stop service naps2-sharing-server");
+        }
+        if (!ProcessHelper.TryRun("systemctl", "--user disable naps2-sharing-server", 1000))
+        {
+            Log.Error("Could not disable service naps2-sharing-server");
+        }
+        try
+        {
+            File.Delete(UnitPath);
+        }
+        catch (Exception ex)
+        {
+            Log.ErrorException($"Error deleting systemd unit: {UnitPath}", ex);
+        }
+        if (!ProcessHelper.TryRun("systemctl", "--user daemon-reload", 1000))
+        {
+            Log.Error("Could not run systemctl daemon-reload");
+        }
     }
 }
